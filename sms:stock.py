de76397144a4e23a0ab67c92f669013a60a47bc3
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-04-11 19:15:24 -0400

    Update sms:stock.py

--- sms:stock.py
+++ sms:stock.py
@@ -9,6 +9,6 @@ price = round(price, 2)
 
 if price > want_price:
     client = nexmo.Client(key='19248f09', secret='cryUpv5JsufkO4Xn')
-    client.send_message({'from': 14256207607, 'to': 18482521431, 'text': '{} is now above {}, it is {}'.format(ticker, want_price, price)})
+    client.send_message({'from': 14256207607, 'to': , 'text': '{} is now above {}, it is {}'.format(ticker, want_price, price)})
 else: 
     print ("want price not met")
