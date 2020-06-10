a97be43ba3417b7e4d8588133eea556fe153c4be
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-06-10 12:31:29 -0400

    Update rsi_indicator.py

--- rsi_indicator.py
+++ rsi_indicator.py
@@ -6,6 +6,7 @@ import pickle
 import bs4 as bs 
 import time 
 import pandas as pd
+from tqdm import tqdm
 
 #Get Dates
 start_date = datetime.datetime.now() - datetime.timedelta(days=365)
@@ -16,23 +17,28 @@ tickers = pd.read_pickle('spxTickers.pickle')
 oversold = []
 overbought = []
 
-for ticker in tickers:
-    try:
-        data = DataReader(ticker, 'yahoo', start_date, end_date)
-        data["rsi"] = talib.RSI(data["Close"])
-        values = data["rsi"].tail(14)
-        value = values.mean()
-        print ('{} has an rsi value of {}'.format(ticker, round(value, 2)))
-        time.sleep(1)
-        
-        if value <= 30:
-            oversold.append(ticker)
+with tqdm(total=len(tickers), desc="RSI Indicator", bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
+    for ticker in tickers:
+        try:
+            data = DataReader(ticker, 'yahoo', start_date, end_date)
+            data["rsi"] = talib.RSI(data["Close"])
+            values = data["rsi"].tail(14)
+            value = values.mean()
+            print ('\n{} has an rsi value of {}'.format(ticker, round(value, 2)))
+            time.sleep(1)
             
-        elif value >= 70:
-            overbought.append(ticker)
-    except Exception as e:
-        print (e)
-        continue
+            if value <= 30:
+                oversold.append(ticker)
+                
+            elif value >= 70:
+                overbought.append(ticker)
+
+        except Exception as e:
+            print (e)
+            continue
         
+        pbar.update(1)
+        print ('-'*50)
+
 print (oversold)
 print (overbought)
