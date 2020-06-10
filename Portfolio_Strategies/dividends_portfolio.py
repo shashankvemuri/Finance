c9bad25c63eaaf22121333cfdeb393c19ba7f530
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-06-10 01:12:42 -0400

    more files

---
+++ Portfolio_Strategies/dividends_portfolio.py
@@ -0,0 +1,157 @@
+# # Stocks Dividends Portfolio
+
+# ## Stocks with Dividend
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# fetch dividend
+import yfinance as yfd
+
+import yfinance as yf
+yf.pdr_override()
+
+
+# input
+symbols = ['ALX','BLK','SPG','LMT']
+start = '2007-01-01'
+end = '2019-01-01'
+
+# Read data 
+df = yf.download(symbols,start,end)['Adj Close']
+
+
+d1 = dt.datetime.strptime(str(start), "%Y-%m-%d")
+d2 = dt.datetime.strptime(str(end), "%Y-%m-%d")
+delta = relativedelta.relativedelta(d2,d1)
+print('How many years of investing?')
+print('%s years' % delta.years)
+
+for s in symbols: 
+    df[s].plot(label = s, figsize = (15,10))
+plt.legend()
+
+for s in symbols:
+    print(s + ":",  df[s].max())
+
+for s in symbols:
+    print(s + ":",  df[s].min())
+
+returns = pd.DataFrame()
+for s in symbols: 
+    returns[s + " Return"] = (np.log(1 + df[s].pct_change())).dropna()
+    
+returns.head(4)
+
+sns.pairplot(returns[1:])
+
+# dates each bank stock had the best and worst single day returns. 
+print('Best Day Returns')
+print('-'*20)
+print(returns.idxmax())
+print('\n')
+print('Worst Day Returns')
+print('-'*20)
+print(returns.idxmin())
+
+plt.figure(figsize=(17,13))
+
+for r in returns:
+    sns.kdeplot(returns.ix["2011-01-01" : "2011-12-31 "][r])
+
+returns.corr()
+
+
+# Heatmap for return of all the banks
+plt.figure(figsize=(15,10))
+sns.heatmap(returns.corr(), cmap="cool",linewidths=.1, annot= True)
+
+sns.clustermap(returns.corr(), cmap="Wistia",linewidths=.1, annot= True)
+
+plt.figure(figsize=(15,10))
+sns.heatmap(df.corr(), cmap="hot",linewidths=.1, annot= True)
+
+sns.clustermap(df.corr(), cmap="copper",linewidths=.1, annot= True)
+
+Cash = 100000
+print('Percentage of invest:')
+percent_invest = [0.25, 0.25, 0.25, 0.25]
+for i, x in zip(df.columns, percent_invest):
+    cost = x * Cash
+    print('{}: {}'.format(i, cost))
+
+print('Number of Shares:')
+percent_invest = [0.25, 0.25, 0.25, 0.25]
+for i, x, y in zip(df.columns, percent_invest, df.iloc[0]):
+    cost = x * Cash
+    shares = int(cost/y)
+    print('{}: {}'.format(i, shares))
+
+print('Beginning Value:')
+percent_invest = [0.25, 0.25, 0.25, 0.25]
+for i, x, y in zip(df.columns, percent_invest, df.iloc[0]):
+    cost = x * Cash
+    shares = int(cost/y)
+    Begin_Value = round(shares * y, 2)
+    print('{}: ${}'.format(i, Begin_Value))
+
+print('Current Value:')
+percent_invest = [0.25, 0.25, 0.25, 0.25]
+for i, x, y, z in zip(df.columns, percent_invest, df.iloc[0], df.iloc[-1]):
+    cost = x * Cash
+    shares = int(cost/y)
+    Current_Value = round(shares * z, 2)
+    print('{}: ${}'.format(i, Current_Value))
+
+result = []
+percent_invest = [0.25, 0.25, 0.25, 0.25]
+for i, x, y, z in zip(df.columns, percent_invest, df.iloc[0], df.iloc[-1]):
+    cost = x * Cash
+    shares = int(cost/y)
+    Current_Value = round(shares * z, 2)
+    result.append(Current_Value)
+print('Total Value: $%s' % round(sum(result),2))
+
+stock = yfd.Tickers('ALX BLK SPG LMT')
+stock
+
+s1_dividend = stock.ALX.dividends['2007-01-01':].sum()
+s2_dividend = stock.BLK.dividends['2007-01-01':].sum()
+s3_dividend = stock.SPG.dividends['2007-01-01':].sum()
+s4_dividend = stock.LMT.dividends['2007-01-01':].sum()
+
+data = [s1_dividend, s2_dividend, s3_dividend, s4_dividend]
+
+print('Total Dividends:')
+data = [s1_dividend, s2_dividend, s3_dividend, s4_dividend]
+for i, x in zip(df.columns, data):
+    print('{}: {}'.format(i, x))
+
+print('Dividends with Shares:')
+percent_invest = [0.25, 0.25, 0.25, 0.25]
+data = [s1_dividend, s2_dividend, s3_dividend, s4_dividend]
+for i, x, y in zip(df.columns, percent_invest, data):
+    cost = x * Cash
+    shares = int(cost/y)
+    total_dividend_cost = shares * y
+    print('{}: ${}'.format(i, round(total_dividend_cost,2)))
+
+dividend = []
+percent_invest = [0.25, 0.25, 0.25, 0.25]
+data = [s1_dividend, s2_dividend, s3_dividend, s4_dividend]
+for i, x, y in zip(df.columns, percent_invest, data):
+    cost = x * Cash
+    shares = int(cost/y)
+    total_dividend_cost = shares * y
+    dividend.append(total_dividend_cost)
+print('Total Dividends: $%s' % round(sum(dividend),2))
+
+print('Total Money: $%s' % round((sum(dividend) + sum(result)),2))
+print('Total Profit: $%s' % (round((sum(dividend) + sum(result)),2) - Cash))
+
---
+++ Portfolio_Strategies/optimal_portfolio.py
@@ -0,0 +1,79 @@
+# # Constructing an Optimal Portfolio
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.optimize import fmin
+import math
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+import yfinance as yf
+yf.pdr_override()
+
+
+# input
+symbols = ['BAC','AAPL', 'JNJ']
+start = '2012-01-01'
+end = '2019-01-01'
+
+rf = 0.003
+
+def annual_returns(symbols, start, end):
+    df = yf.download(symbols,start,end)['Adj Close']
+    log_rets = np.log(df) - np.log(df.shift(1))
+    date = []
+    d0 = df.index
+    for i in range(0, len(log_rets)):
+        date.append(d0[i].strftime("%Y"))
+    y = pd.DataFrame(log_rets, date, columns = [symbols])
+    return np.exp(y.groupby(y.index).sum()) - 1
+
+def portfolio_var(M, W):
+    cor = np.corrcoef(M.T)
+    vol = np.std(M, axis=0)
+    var = 0.0
+    for i in range(n):
+        for j in range(n):
+            var += W[i] * W[j] * vol[i] * vol[j] * cor[i, j]
+    return var
+
+def sharpe(M, W):
+    var = portfolio_var(M, W)
+    mean_return = np.mean(M, axis=0)
+    ret = np.array(mean_return)
+    return (np.dot(W, ret) - rf)/ np.sqrt(252)
+
+def negative_sharpe_n_minus_1_stock(W):
+    w2 = np.append(W, 1-sum(W))
+    return -sharpe(M, w2)
+
+n = len(symbols)
+x2 = annual_returns(symbols[0], start, end)
+for i in range(1,n):
+    x_ = annual_returns(symbols[i], start, end)
+    x2 = pd.merge(x2, x_, left_index=True, right_index=True)
+    
+M = np.array(x2)
+
+print('Efficient Portfolio (Mean-Variance)')
+print('Symbols: ', symbols)
+print('Sharpe ratio for an equal-weighted portfolio')
+equal_weighted = np.ones(n, dtype=float) * 1.0/n
+print(equal_weighted)
+print(round(sharpe(M, equal_weighted), 4))
+
+w0 = np.ones(n-1, dtype=float) * 1.0 / n
+w1 = fmin(negative_sharpe_n_minus_1_stock, w0)
+
+final_weight = np.append(w1, 1 - sum(w1))
+final_sharpe = sharpe(M, final_weight)
+
+print('Optimal weights:')
+print(final_weight)
+print('Sharpe ratio:')
+print(round(final_sharpe,4))
+
