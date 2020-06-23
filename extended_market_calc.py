96b33bdaab4fe32f1294feb7401774ab0d243c3a
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-06-23 13:50:46 -0400

    add more files

--- extended_market_calc.py
+++ extended_market_calc.py
@@ -1,15 +1,14 @@
 import yfinance as yf
 import datetime as dt
-import pandas as pd
 from pandas_datareader import data as pdr
 import matplotlib.pyplot as plt
-import statistics
 import numpy as np
 import matplotlib.ticker as mticker
+from pylab import rcParams
 
 yf.pdr_override() 
 
-num_of_years = 3
+num_of_years = 40
 start = dt.datetime.now() - dt.timedelta(int(365.25 * num_of_years))
 now = dt.datetime.now() 
 
@@ -45,7 +44,7 @@ while stock != "quit":
     
     # fixed bin size
     bins = np.arange(-100, 100, 1) 
-    
+    rcParams['figure.figsize'] = 15, 10
     plt.xlim([df["PC"].min()-5, df["PC"].max()+5])
     
     plt.hist(df["PC"], bins=bins, alpha=0.5)
@@ -61,8 +60,8 @@ while stock != "quit":
     plt.axvline( x=-2*stdev+mean, ymin=0, ymax=1, color='gray',alpha=.75, linestyle='--')
     plt.axvline( x=-3*stdev+mean, ymin=0, ymax=1, color='gray', alpha=.5, linestyle='--')
     
-    plt.axvline( x=current, ymin=0, ymax=1, color='r')
-    plt.axvline( x=yday, ymin=0, ymax=1, color='blue')
+    plt.axvline( x=current, ymin=0, ymax=1, color='r', label = 'today')
+    plt.axvline( x=yday, ymin=0, ymax=1, color='blue', label = 'yesterday')
     
     #add more x axis labels
     ax1.xaxis.set_major_locator(mticker.MaxNLocator(14)) 
@@ -80,6 +79,7 @@ while stock != "quit":
     #add more x axis labels
     ax2.xaxis.set_major_locator(mticker.MaxNLocator(8)) 
     plt.axhline( y=limit, xmin=0, xmax=1, color='r')
+    rcParams['figure.figsize'] = 15, 10
     plt.show()
     
     stock = input("Enter the stock symbol : ") 
