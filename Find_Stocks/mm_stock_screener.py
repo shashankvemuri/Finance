859dbc3378c90c2ac725c90cac165c7cc26b251b
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-07-11 16:52:23 -0400

    change and add some files

--- Find_Stocks/mm_stock_screener.py
+++ Find_Stocks/mm_stock_screener.py
@@ -1,67 +1,56 @@
-import datetime
-import pandas as pd
 from pandas_datareader import data as pdr
-import yfinance as yf
-from pandas import ExcelWriter
-import requests
 from yahoo_fin import stock_info as si
+from pandas import ExcelWriter
+import yfinance as yf
+import pandas as pd
+import requests
+import datetime
 import time
-import bs4 as bs
-import pickle
-import talib
+import os
 
 yf.pdr_override()
 
-def save_spx_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    soup = bs.BeautifulSoup(resp.text, 'lxml')
-    table = soup.find('table', {'class':'wikitable sortable'})
-    tickers = []
-    for row in table.findAll('tr')[1:]:
-        ticker = row.find_all('td') [0].text.strip()
-        tickers.append(ticker)
-        
-    with open('spxTickers.pickle', 'wb') as f:
-            pickle.dump(tickers, f)       
-    return tickers
-        
-stocklist = save_spx_tickers()
-
-# Make the ticker symbols readable by Yahoo Finance
+stocklist = si.tickers_sp500()
 stocklist = [item.replace(".", "-") for item in stocklist]
+
+index_name = '^GSPC' # S&P 500
+
+final = []
+index = []
+n = -1
 
 mylist = []
 today = datetime.date.today()
 mylist.append(today)
 today = mylist[0]
 
-final = []
-index = []
-n = -1
-
-exportList = pd.DataFrame(columns=['Stock', "RSI", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
-otherList = pd.DataFrame(columns=['Stock', "RSI", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High", "Failed"])
+exportList = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
+otherList = pd.DataFrame(columns=['Stock', "RS_Rating", "50 Day MA", "150 Day Ma", "200 Day MA", "52 Week Low", "52 week High"])
 
 for stock in stocklist:
-    #n += 1
-    time.sleep(1.5)
+    n += 1
+    time.sleep(1)
     
-    print ("\npulling {}".format(stock))
-    # rsi value
+    print ("\npulling {} with index {}".format(stock, n))
+
+    # RS_Rating 
     start_date = datetime.datetime.now() - datetime.timedelta(days=365)
     end_date = datetime.date.today()
     
     df = pdr.get_data_yahoo(stock, start=start_date, end=end_date)
+    df['Percent Change'] = df['Adj Close'].pct_change()    
+    stock_return = df['Percent Change'].sum() * 100
     
-    df["rsi"] = talib.RSI(df["Close"])
+    index_df = pdr.get_data_yahoo(index_name, start=start_date, end=end_date)
+    index_df['Percent Change'] = index_df['Adj Close'].pct_change()
+    index_return = index_df['Percent Change'].sum() * 100
     
-    RSI = df["rsi"].tail(14).mean()
-
+    RS_Rating = round((stock_return / index_return) * 10, 2)
+    
     try:
-        smaUsed = [50, 150, 200]
-        for x in smaUsed:
-            sma = x
-            df["SMA_"+str(sma)] = round(df.iloc[:,4].rolling(window=sma).mean(), 2)
+        sma = [50, 150, 200]
+        for x in sma:
+            df["SMA_"+str(x)] = round(df.iloc[:,4].rolling(window=x).mean(), 2)
 
         currentClose = df["Adj Close"][-1]
         moving_average_50 = df["SMA_50"][-1]
@@ -113,15 +102,23 @@ for stock in stocklist:
             condition_7 = True
         else:
             condition_7 = False
-        # Condition 8: IBD RS rating < 30 and the lower the better
-        if(RSI < 30):
+            
+        # Condition 8: IBD RS_Rating greater than 70
+        if(RS_Rating >= 70):
             condition_8 = True
         else:
             condition_8 = False
 
-        if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8):            
-            exportList = exportList.append({'Stock': stock, "RSI": RSI, "50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}, ignore_index=True)
-            #print (stock + " made the requirements")
+        if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8):
+            final.append(stock)
+            index.append(n)
+            
+            dataframe = pd.DataFrame(list(zip(final, index)), columns =['Company', 'Index'])
+            
+            dataframe.to_csv('/Users/shashank/Documents/Code/Python/Outputs/mm-screener-output/stocks.csv')
+            
+            exportList = exportList.append({'Stock': stock, "RS_Rating": RS_Rating ,"50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}, ignore_index=True)
+            print (stock + " made the requirements")
         
         else:
             conditions = {'condition_1': condition_1, 'condition_2': condition_2, 'condition_3': condition_3, 'condition_4': condition_4, 'condition_5': condition_5, 'condition_6': condition_6, 'condition_7': condition_7, 'condition_8': condition_8}
@@ -137,18 +134,17 @@ for stock in stocklist:
             for value in false: 
                 print (value)
             
-            otherList = otherList.append({'Stock': stock, "RSI": RSI, "50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week, "Failed": false}, ignore_index=True)
-
+            otherList = otherList.append({'Stock': stock, "RS_Rating": RS_Rating ,"50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}, ignore_index=True)
     except Exception as e:
-        pass
-        #print (e)
-        #print("No data on "+stock)
+        print (e)
+        print("No data on "+stock)
 
-#print(exportList)
-writer = ExcelWriter('/Users/shashank/Documents/GitHub/Code/mm-screener-output/Export-Output_{}.xlsx'.format(today))
+print(exportList)
+
+writer = ExcelWriter('/Users/shashank/Documents/Code/Python/Outputs/mm-screener-output/Export-Output_{}.xlsx'.format(today))
 exportList.to_excel(writer, "Sheet1")
 writer.save()
 
-writer = ExcelWriter('/Users/shashank/Documents/GitHub/Code/mm-screener-output/Other-Output_{}.xlsx'.format(today))
+writer = ExcelWriter('/Users/shashank/Documents/Code/Python/Outputs/mm-screener-output/Other-Output_{}.xlsx'.format(today))
 otherList.to_excel(writer, "Sheet1")
 writer.save()
