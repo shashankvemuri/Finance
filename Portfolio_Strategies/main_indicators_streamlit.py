affedc261ccf9f8ce299d9930028745e8a189f70
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-07-20 15:13:02 -0400

    add and remove files

--- Portfolio_Strategies/main_indicators_streamlit.py
+++ Portfolio_Strategies/main_indicators_streamlit.py
@@ -1,28 +1,50 @@
-import matplotlib.pyplot as plt
-from pylab import rcParams
 import yfinance as yf
-import datetime as dt
-import warnings
+import streamlit as st
+import datetime 
+import matplotlib.pyplot as plt
 import talib 
 import ta
 import numpy as np
 import matplotlib.ticker as mticker
-from pandas_datareader import data as pdr
-
-warnings.filterwarnings("ignore")
+import pandas as pd
+import requests
 yf.pdr_override()
 
-# input
-symbol = str(input('Enter a ticker: '))
-symbol = symbol.strip()
-symbol = symbol.upper()
+st.write("""
+# Technical Analysis Web Application
+Shown below are the **Moving Average Crossovers**, **Bollinger Bands**, **MACD's**, **Commodity Channel Indexes**, **Relative Strength Indexes** and **Extended Market Calculators** of any stock!
+""")
 
-num_of_years = 1
-start = dt.date.today() - dt.timedelta(days = int(365.25 * num_of_years))
-end = dt.date.today()
+st.sidebar.header('User Input Parameters')
+
+today = datetime.date.today()
+def user_input_features():
+    ticker = st.sidebar.text_input("Ticker", 'AAPL')
+    start_date = st.sidebar.text_input("Start Date", '2019-01-01')
+    end_date = st.sidebar.text_input("End Date", f'{today}')
+    return ticker, start_date, end_date
+
+symbol, start, end = user_input_features()
+
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+    result = requests.get(url).json()
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == symbol:
+            return x['name']
+company_name = get_symbol(symbol.upper())
+
+start = pd.to_datetime(start)
+end = pd.to_datetime(end)
 
 # Read data 
 data = yf.download(symbol,start,end)
+
+# Adjusted Close Price
+st.header(f"""
+          Adjusted Close Price\n {company_name}
+          """)
+st.line_chart(data['Adj Close'])
 
 # ## SMA and EMA
 #Simple Moving Average
@@ -31,88 +53,60 @@ data['SMA'] = talib.SMA(data['Adj Close'], timeperiod = 20)
 # Exponential Moving Average
 data['EMA'] = talib.EMA(data['Adj Close'], timeperiod = 20)
 
+# Plot
+st.header(f"""
+          Simple Moving Average vs. Exponential Moving Average\n {company_name}
+          """)
+st.line_chart(data[['Adj Close','SMA','EMA']])
+
 # Bollinger Bands
 data['upper_band'], data['middle_band'], data['lower_band'] = talib.BBANDS(data['Adj Close'], timeperiod =20)
 
 # Plot
-rcParams['figure.figsize'] = 15,10
-fig, (ax1, ax2) = plt.subplots(2)
-ax1.plot(data[['Adj Close','SMA','EMA']])
-ax1.legend(('Adj Close', 'SMA', 'EMA'))
-ax1.set_ylabel('Close Price')
-ax1.set_title(f'SMA vs. EMA for {symbol.upper()}')
-
-ax2.plot(data[['Adj Close','upper_band','middle_band','lower_band']])
-ax2.legend(('Adj Close', 'Upper Band', 'Middle Band', 'Lower Band'))
-ax2.set_xlabel('Dates')
-ax2.set_ylabel('Adj Close Price')
-ax2.set_title(f'Bollinger Bands for {symbol.upper()}')
-ax2.fill_between(data.index, y1=data['lower_band'], y2=data['upper_band'], color = 'lightcoral', alpha=0.3)
-plt.show()
-
+st.header(f"""
+          Bollinger Bands\n {company_name}
+          """)
+st.line_chart(data[['Adj Close','upper_band','middle_band','lower_band']])
 
 # ## MACD (Moving Average Convergence Divergence)
 # MACD
 data['macd'], data['macdsignal'], data['macdhist'] = talib.MACD(data['Adj Close'], fastperiod=12, slowperiod=26, signalperiod=9)
 
 # Plot
-rcParams['figure.figsize'] = 15,10
-fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle(f'Close Price vs. Commodity Channel Index for {symbol.upper()}')
-ax1.plot(data['Adj Close'])
-ax1.set_ylabel('Adj Close')
-
-ax2.plot(data[['macd','macdsignal']])
-ax2.legend(('MACD', 'MACD Signal'))
-ax2.set_ylabel('MACD')
-ax2.set_title(f'Moving Average Convergence Divergence for {symbol.upper()}')
+st.header(f"""
+          Moving Average Convergence Divergence\n {company_name}
+          """)
+st.line_chart(data[['macd','macdsignal']])
 
 ## CCI (Commodity Channel Index)
 # CCI
 cci = ta.trend.cci(data['High'], data['Low'], data['Close'], n=31, c=0.015)
-print (cci)
 
-rcParams['figure.figsize'] = 15,10
-fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle(f'Close Price vs. Commodity Channel Index for {symbol.upper()}')
-ax1.plot(data['Adj Close'])
-ax1.set_ylabel('Adj Close')
-
-ax2.plot(cci)
-ax2.axhline(y=0, color='r', linestyle='-')
-ax2.set_xlabel('Dates')
-ax2.set_ylabel('Commodity Channel Index')
-ax2.set_title(f'Commodity Channel Index for {symbol.upper()}')
-plt.show()
-
+# Plot
+st.header(f"""
+          Commodity Channel Index\n {company_name}
+          """)
+st.line_chart(cci)
 
 # ## RSI (Relative Strength Index)
 # RSI
 data['RSI'] = talib.RSI(data['Adj Close'], timeperiod=14)
-fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle(f'Close Price vs. Relative Strength Index for {symbol.upper()}')
-ax1.plot(data['Adj Close'])
-ax1.set_ylabel('Adj Close')
 
-ax2.plot(data['RSI'])
-ax2.fill_between(data.index, y1=30, y2=70, color = 'lightcoral', alpha=0.3)
-ax2.set_ylabel('RSI')
-plt.show()
+# Plot
+st.header(f"""
+          Relative Strength Index\n {company_name}
+          """)
+st.line_chart(data['RSI'])
 
 # ## OBV (On Balance Volume)
 # OBV
 data['OBV'] = talib.OBV(data['Adj Close'], data['Volume'])/10**6
 
-plt.gcf()
-fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle(f'Close Price vs. On Balance Volume for {symbol.upper()}')
-ax1.plot(data['Adj Close'])
-ax1.set_ylabel('Adj Close')
-
-ax2.plot(data['OBV'])
-ax2.set_ylabel('On Balance Volume (in millions)')
-plt.show()
-
+# Plot
+st.header(f"""
+          On Balance Volume\n {company_name}
+          """)
+st.line_chart(data['OBV'])
 
 # Extended Market
 fig, ax1 = plt.subplots() 
@@ -121,23 +115,30 @@ fig, ax1 = plt.subplots()
 sma = 50
 limit = 10
 
+data = yf.download(symbol,start, today)
+
 #calculates sma and creates a column in the dataframe
 data['SMA'+str(sma)] = data.iloc[:,4].rolling(window=sma).mean() 
 data['PC'] = ((data["Adj Close"]/data['SMA'+str(sma)])-1)*100
 
-mean =data["PC"].mean()
-stdev=data["PC"].std()
-current=data["PC"][-1]
-yday=data["PC"][-2]
+mean = round(data["PC"].mean(), 2)
+stdev = round(data["PC"].std(), 2)
+current= round(data["PC"][-1], 2)
+yday= round(data["PC"][-2], 2)
 
-print(str(current))
+stats = [['Mean', mean], ['Standard Deviation', stdev], ['Current', current], ['Yesterday', yday]]
 
-print("Mean: "+str(mean))
-print("Standard Dev: "+str(stdev))
+frame = pd.DataFrame(stats,
+                   columns = ['Statistic', 'Value'])
+
+st.header(f"""
+          Extended Market Calculator\n {company_name}
+          """)
+st.dataframe(frame.style.hide_index())
 
 # fixed bin size
 bins = np.arange(-100, 100, 1) 
-rcParams['figure.figsize'] = 15, 10
+plt.rcParams['figure.figsize'] = 15, 10
 plt.xlim([data["PC"].min()-5, data["PC"].max()+5])
 
 plt.hist(data["PC"], bins=bins, alpha=0.5)
@@ -159,6 +160,8 @@ plt.axvline( x=yday, ymin=0, ymax=1, color='blue', label = 'yesterday')
 #add more x axis labels
 ax1.xaxis.set_major_locator(mticker.MaxNLocator(14)) 
 
+st.pyplot(fig)
+
 #Create Plots
 fig2, ax2 = plt.subplots() 
 
@@ -172,5 +175,5 @@ plt.ylabel('Percent from '+str(sma)+' EMA')
 #add more x axis labels
 ax2.xaxis.set_major_locator(mticker.MaxNLocator(8)) 
 plt.axhline( y=limit, xmin=0, xmax=1, color='r')
-rcParams['figure.figsize'] = 15, 10
-plt.show()
+plt.rcParams['figure.figsize'] = 15, 10
+st.pyplot(fig2)
---
+++ Portfolio_Strategies/pairs_trading.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+import seaborn
+import statsmodels
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import coint
+import datetime
+from pandas_datareader import data as pdr
+
+yf.pdr_override()
+
+num_of_years = .5
+start = datetime.date.today() - datetime.timedelta(days = int(365.25 * num_of_years))
+end = datetime.date.today()
+
+
+stocks =['A', 'AAPL', 'MSFT', 'INTL', 'AMZN', 'NFLX', 'IBM', 'NVDA', 'CSCO', 'BABA', 'AMD', 'SPY']
+data = pdr.get_data_yahoo(stocks, start, end)['Close']
+data = data.dropna()
+
+def find_cointegrated_pairs(data):
+    n = data.shape[1]
+    score_matrix = np.zeros((n, n))
+    pvalue_matrix = np.ones((n, n))
+    keys = data.keys()
+    pairs = []
+    for i in range(n):
+        for j in range(i+1, n):
+            S1 = data[keys[i]]
+            S2 = data[keys[j]]
+            result = coint(S1, S2)
+            score = result[0]
+            pvalue = result[1]
+            score_matrix[i, j] = score
+            pvalue_matrix[i, j] = pvalue
+            if pvalue < 0.01:
+                pairs.append((keys[i], keys[j]))
+    return score_matrix, pvalue_matrix, pairs
+
+scores, pvalues, pairs = find_cointegrated_pairs(data)
+seaborn.heatmap(pvalues, xticklabels = data.columns, yticklabels = data.columns, cmap = 'plasma', mask = (pvalues >= 0.01))
+plt.rcParams['figure.figsize'] = (15, 10)
+plt.title('Heatmap of Co-Integrated Stocks')
+plt.subplots()
+plt.show()
+
+#normalize stock prices
+def zscore(stocks):
+    return (stocks - stocks.mean())/np.std(stocks)
+
+if len(pairs) == 0:
+    print ('There were no pairs in the inputted list!')
+
+else:
+    coint_pairs = pairs
+    values = []
+    
+    n = 0
+    for i in coint_pairs:
+        S1 = data[i[0]]
+        S2 = data[i[1]]
+        score, pvalue, _ = coint(S1, S2)
+        print(f'p-value between {i[0]} and {i[1]}: ' + str(pvalue))
+        values.append(pvalue)
+    
+    min_val = np.argmin(values)
+    spread = data[pairs[min_val][0]] - data[pairs[min_val][1]]
+    
+    spread_mavg1 = spread.rolling(1).mean()
+    spread_mavg20 = spread.rolling(20).mean()
+    spread_std20 = spread.rolling(20).std()
+    zscore_20_1 = (spread_mavg1 - spread_mavg20)/spread_std20
+    plt.rcParams['figure.figsize'] = (15, 10)
+    zscore_20_1.plot(label = 'Rolling 20 day z score')
+    plt.axhline(0,color = 'black')
+    plt.axhline(1, c ='r',ls='--')
+    plt.axhline(-1, c ='r',ls='--')
+    plt.title(f'Rolling 20-Day Spread Between {pairs[min_val][0]} and {pairs[min_val][1]}')
+    plt.xlabel('Dates')
+    plt.ylabel('Spread')
+    plt.show()
