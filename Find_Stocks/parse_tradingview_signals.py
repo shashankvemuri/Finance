b80ade2a2c21323049dd47e0a913ef9f65def093
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-07-24 14:42:48 -0400

    updates

--- Find_Stocks/parse_tradingview_signals.py
+++ Find_Stocks/parse_tradingview_signals.py
@@ -2,63 +2,80 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-'''
-Intervals:
-"1m" for 1 minute.
-"5m" for 5 minutes.
-"15m" for 15 minutes.
-"1h" for 1 hour.
-"4h" for 4 hours.
-"1D" for 1 day.
-"1W" for 1 week.
-"1M" for 1 month.
-'''
+import pandas as pd
+import datetime
+import time
 
 interval = '1M'
-ticker = 'MRK'
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument('window-size=1200x600')
-webdriver = webdriver.Chrome(executable_path='/Users/shashank/Documents/Code/Python/Finance/chromedriver.exe', options = options)
+webdriver = webdriver.Chrome(executable_path='/Users/shashank/Documents/Code/Python/Finance/chromedriver', options = options)
 
-#Declare variable
-analysis = []
+tickers = pd.read_pickle('../spxTickers.pickle')
 
-#Open tradingview's site
-webdriver.get("https://s.tradingview.com/embed-widget/technical-analysis/?locale=en#%7B%22interval%22%3A%22{}%22%2C%22width%22%3A%22100%25%22%2C%22isTransparent%22%3Afalse%2C%22height%22%3A%22100%25%22%2C%22symbol%22%3A%22{}%22%2C%22showIntervalTabs%22%3Atrue%2C%22colorTheme%22%3A%22dark%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22technical-analysis%22%7D".format(interval, ticker))
-webdriver.refresh()
+mylist = []
+today = datetime.date.today()
+mylist.append(today)
+today = mylist[0]
 
-#Wait for site to load elements
-while len(webdriver.find_elements_by_class_name("speedometerSignal-pyzN--tL")) == 0:
-    sleep(0.1)
+signals = []
+sells = []
+buys = []
+neutrals = []
+valid_tickers = []
 
-#Recommendation
-recommendation_element = webdriver.find_element_by_class_name("speedometerSignal-pyzN--tL")
-analysis.append(recommendation_element.get_attribute('innerHTML'))
-
-#Counters
-counter_elements = webdriver.find_elements_by_class_name("counterNumber-3l14ys0C")
-
-#Sell
-analysis.append(int(counter_elements[0].get_attribute('innerHTML')))
-
-#Neutral
-analysis.append(int(counter_elements[1].get_attribute('innerHTML')))
-
-#Buy
-analysis.append(int(counter_elements[2].get_attribute('innerHTML')))
-
-last_analysis = analysis
-signal = last_analysis[0]
-num_sell = last_analysis[1]
-num_neutral = last_analysis[2]
-num_buy = last_analysis[3]
-
-print (f'TradingView Data for {ticker} for {interval}: ')
-print ('-'*35)
-print (f'Overall Signal: {signal}')
-print (f'Number of Sell Indicators: {num_sell}')
-print (f'Number of Neutral Indicators: {num_neutral}')
-print (f'Number of Buy Indicators: {num_buy}')
+for ticker in tickers:
+    try:
+        #Declare variable
+        analysis = []
+        
+        #Open tradingview's site
+        webdriver.get("https://s.tradingview.com/embed-widget/technical-analysis/?locale=en#%7B%22interval%22%3A%22{}%22%2C%22width%22%3A%22100%25%22%2C%22isTransparent%22%3Afalse%2C%22height%22%3A%22100%25%22%2C%22symbol%22%3A%22{}%22%2C%22showIntervalTabs%22%3Atrue%2C%22colorTheme%22%3A%22dark%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22technical-analysis%22%7D".format(interval, ticker))
+        webdriver.refresh()
+        
+        #Wait for site to load elements
+        while len(webdriver.find_elements_by_class_name("speedometerSignal-pyzN--tL")) == 0:
+            sleep(0.1)
+        
+        #Recommendation
+        recommendation_element = webdriver.find_element_by_class_name("speedometerSignal-pyzN--tL")
+        analysis.append(recommendation_element.get_attribute('innerHTML'))
+        
+        #Counters
+        counter_elements = webdriver.find_elements_by_class_name("counterNumber-3l14ys0C")
+        
+        #Sell
+        analysis.append(int(counter_elements[0].get_attribute('innerHTML')))
+        
+        #Neutral
+        analysis.append(int(counter_elements[1].get_attribute('innerHTML')))
+        
+        #Buy
+        analysis.append(int(counter_elements[2].get_attribute('innerHTML')))
+        
+        last_analysis = analysis
+        signal = last_analysis[0]
+        num_sell = float(last_analysis[1])
+        num_neutral = float(last_analysis[2])
+        num_buy = float(last_analysis[3])
+        
+        signals.append(signal)
+        neutrals.append(num_neutral)
+        buys.append(num_buy)
+        sells.append(num_sell)
+        valid_tickers.append(ticker)
+        
+        print (f'{ticker} has an overall recommendation of {signal}')
+        print ()
+        
+        time.sleep(1)
+        
+    except:
+        continue
+    
+dataframe = pd.DataFrame(list(zip(valid_tickers, signals, buys, sells, neutrals)), columns =['Tickers', 'Signals', 'Buys', 'Sells', 'Neutrals'])
+dataframe.to_csv(f'/Users/shashank/Documents/Code/Python/Outputs/tradingview/{today}.csv')
+dataframe = dataframe.sort_values('Buys', ascending=False)
+print (dataframe)
--- Find_Stocks/scrape_reddit.py
+++ Find_Stocks/scrape_reddit.py
@@ -7,15 +7,24 @@ pd.set_option('display.max_columns', None)
 my_client_id, my_client_secret, my_user_agent = get_reddit()
 reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret, user_agent=my_user_agent)
 
-# get 10 top posts from the fatFIRE subreddit
-top_posts = reddit.subreddit('fatFIRE').top(limit=1)
-for post in top_posts:
-    print(post.title)
-    print(post.selftext)
+sub = 'fatFIRE'
+num_of_articles = 5
+
+# # get posts from subreddit
+# wanted_posts = reddit.subreddit(sub).new(limit = num_of_articles)
+# for post in wanted_posts:
+#     print(post.title)
+#     print(post.selftext)
 
 posts = []
-subreddit = reddit.subreddit('fatFIRE')
-for post in subreddit.top(limit=10):
+subreddit = reddit.subreddit(sub)
+for post in subreddit.new(limit = num_of_articles):
     posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
 posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
-# print(posts)
+posts = posts.drop(columns = ['score', 'id', 'subreddit', 'created', 'url'])
+# posts = posts.set_index('title')
+print(posts)
+
+print ()
+print (posts['title'].tolist()[4])
+print (posts['body'].tolist()[4])
