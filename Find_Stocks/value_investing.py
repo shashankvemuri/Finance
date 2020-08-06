5866f2e90164f4bbb07f9ab2344730b73274828f
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-08-05 22:16:41 -0400

    add and change files

--- Find_Stocks/value_investing.py
+++ Find_Stocks/value_investing.py
@@ -1,69 +1,95 @@
 import pandas as pd
+from selenium import webdriver
+import time
 from bs4 import BeautifulSoup as bs
-import requests
-import pickle
+from selenium.webdriver.chrome.options import Options
+from yahoo_fin import stock_info as si
+import datetime 
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-'''            try:
-                df.loc[symbol, m] = fundamental_metric(soup, m)
-            except Exception as e:
-                print(symbol, 'not found')
-                print(e)
-                break
-'''
+mylist = []
+today = datetime.date.today()
+mylist.append(today)
+today = mylist[0]
 
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome('../chromedriver', options=options)
 
-# Define The Method To Extract Fundamental Data
-def get_fundamental_data(df):
-    for symbol in df.index:
-
-        url = ("http://finviz.com/quote.ashx?t=" + symbol.lower())
-        soup = bs(requests.get(url).content, features='html5lib')
-        for m in df.columns:
-            df.loc[symbol, m] = fundamental_metric(soup, m)
-    return df
-
-
-def fundamental_metric(soup, metric):
-    return soup.find(text=metric).find_next(class_='snapshot-td2-cp').text
-
-# Define A List Of Stocks And The Fundamental Metrics
-# save_sp500_tickers()
-def save_spx_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    soups = bs(resp.text, 'lxml')
-    tables = soups.find('table', {'class':'wikitable sortable'})
-    tickers = []
-    for row in tables.findAll('tr')[1:]:
-        ticker = row.find_all('td') [0].text.strip()
-        tickers.append(ticker)
+def finviz_parser(stock_list, metric):
+    def get_fundamental_data(df):
+        for symbol in df.index:
+            print (symbol)
+            try:
+                url = ("http://finviz.com/quote.ashx?t=" + symbol.lower())
+                driver.get(url)
+                time.sleep(.5)
+                
+                html = driver.page_source
+                soup = bs(html, features="lxml")
+                for m in df.columns:
+                    df.loc[symbol, m] = fundamental_metric(soup, m)
+            except:
+                continue
+        return df
         
-    with open('spxTickers.pickle', 'wb') as f:
-            pickle.dump(tickers, f)       
-    return tickers
+    def fundamental_metric(soup, metric):
+        return soup.find(text=metric).find_next(class_='snapshot-td2').text
+    
+    df = pd.DataFrame(index=stock_list, columns=metric)
+    df = get_fundamental_data(df)
+    print("All stocks with fundamental data: ")
+    print(df)
+    
+    if stock_list == si.tickers_dow():
+        path = f'/Users/shashank/Documents/Code/Python/Outputs/finviz_fundamentals/dow/{today}.csv'
+        df.to_csv(path)
+    elif stock_list == si.tickers_sp500():
+        path = f'/Users/shashank/Documents/Code/Python/Outputs/finviz_fundamentals/sp500/{today}.csv'
+        df.to_csv(path)
+    else:
+        path = f'/Users/shashank/Documents/Code/Python/Outputs/finviz_fundamentals/{today}.csv'
+        df.to_csv(path)
         
-tickers = save_spx_tickers()
-tickers = [item.replace(".", "-") for item in tickers]
-
-stock_list = tickers 
-
-metric = ['P/B',
+    driver.close()
+    
+stock_list = pd.read_csv('/Users/shashank/Documents/Code/Python/Research/nasdaq100/nasdaq100_tickers.csv')['Ticker'].tolist()
+metric = ['Dividend',
+          'Dividend %',
           'P/E',
           'Forward P/E',
           'PEG',
+          'P/B',
           'Debt/Eq',
           'EPS (ttm)',
           'Dividend %',
           'ROE',
           'ROI',
           'EPS Q/Q',
-          'Insider Own'
+          'EPS (ttm)',
+          'Earnings',
+          'Insider Own',
+          'Quick Ratio',
+          'Current Ratio',
+          'Short Ratio',
+          'Volatility',
+          'ATR',
+          'Beta'
           ]
+finviz_parser(stock_list, metric)
 
-df = pd.DataFrame(index=stock_list, columns=metric)
-df = get_fundamental_data(df)
-print("All stocks with fundamental data")
-print(df.head())
-df.to_csv('/Users/shashank/Downloads/fundamental_data.csv')
+# =============================================================================
+# if stock_list == si.tickers_dow():
+#     path = f'/Users/shashank/Documents/Code/Python/Outputs/finviz_fundamentals/dow/{today}.csv'
+#     df = pd.read_csv(path, index_col=0)
+# elif stock_list == si.tickers_sp500():
+#     path = f'/Users/shashank/Documents/Code/Python/Outputs/finviz_fundamentals/sp500/{today}.csv'
+#     df = pd.read_csv(path, index_col=0)
+# else:
+#     path = f'/Users/shashank/Documents/Code/Python/Outputs/finviz_fundamentals/{today}.csv'
+#     df = pd.read_csv(path, index_col=0)
+#  
+# print (df)
+# =============================================================================
