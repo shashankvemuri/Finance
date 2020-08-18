f9c5e9cbfa228042e26179a84b7082af45da2d9f
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-08-18 00:48:02 -0400

    updates

--- Stock_Analysis/time_returns_analysis.py
+++ Stock_Analysis/time_returns_analysis.py
@@ -1,39 +1,31 @@
-# # Annual Returns & Monthly Returns
-import numpy as np
-import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
 import yfinance as yf
 yf.pdr_override()
 from matplotlib import dates as mdates
-import calendar
-import datetime
+import datetime as dt
 
 # input
 symbol = 'AAPL'
-start = dt.date.today() - dt.timedelta(days = 365*10)
+start = dt.date.today() - dt.timedelta(days = 365*5)
 end = dt.date.today()
 
 # Read data 
 dataset = yf.download(symbol,start,end)
 
-# View Data
-dataset.head()
-dataset.tail()
-
-plt.figure(figsize=(16,8))
+plt.figure(figsize=(15,10))
 plt.plot(dataset['Adj Close'])
 plt.title('Closing Price Chart')
 plt.xlabel('Date')
 plt.ylabel('Price')
 plt.grid(True)
+plt.subplots()
 plt.show()
-
 
 monthly = dataset.asfreq('BM')
 monthly['Returns'] = dataset['Adj Close'].pct_change().dropna()
-monthly.head()
 
 monthly['Month_Name'] = monthly.index.strftime("%b")
 monthly['Month_Name_Year'] = monthly.index.strftime("%b-%Y")
@@ -41,21 +33,13 @@ monthly['Month_Name_Year'] = monthly.index.strftime("%b-%Y")
 monthly = monthly.reset_index()
 monthly['Month'] = monthly["Date"].dt.month
 
-monthly.head()
-
-monthly.head()
-
-monthly.tail()
-
-
-monthly['Returns'].plot(kind='bar', figsize=(30,6))
+monthly['Returns'].plot(kind='bar', figsize=(15,10))
 plt.xlabel("Months")
 plt.ylabel("Returns")
 plt.title("Returns for Each Month")
-plt.subplots()
 plt.show()
 
-monthly['Returns'].plot(kind='bar', figsize=(30,6))
+monthly['Returns'].plot(kind='bar', figsize=(15,10))
 plt.xlabel("Months")
 plt.ylabel("Returns")
 plt.title("Returns for Each Month")
@@ -67,37 +51,27 @@ monthly['Date'] = pd.to_datetime(monthly['Date'])
 monthly['Date'] = monthly['Date'].apply(mdates.date2num)
 
 colors = monthly.ReturnsPositive.map({True: 'g', False: 'r'})
-monthly['Returns'].plot(kind='bar', color = colors, figsize=(30,6))
+monthly['Returns'].plot(kind='bar', color = colors, figsize=(15,10))
 plt.xlabel("Months")
 plt.ylabel("Returns")
-plt.title("Returns for Each Month " + start + ' to ' + end)
+plt.title("Returns for Each Month " + str(start) + ' to ' + str(end))
 plt.xticks(monthly.index, monthly['Month_Name'])
 plt.show()
 
 yearly = dataset.asfreq('BY')
 yearly['Returns'] = dataset['Adj Close'].pct_change().dropna()
-
-yearly
-
 yearly = yearly.reset_index()
-yearly
-
 yearly['Years'] = yearly['Date'].dt.year
-yearly
-
-plt.figure(figsize=(10,5))
-plt.bar(yearly['Years'], yearly['Returns'], align='center')
-plt.title('Yearly Returns')
-plt.xlabel('Date')
-plt.ylabel('Returns')
-plt.show()
+print(yearly)
 
 yearly['ReturnsPositive'] = 0 < yearly['Returns']
 yearly['Date'] = pd.to_datetime(yearly['Date'])
 yearly['Date'] = yearly['Date'].apply(mdates.date2num)
 
 colors = yearly.ReturnsPositive.map({True: 'g', False: 'r'})
-plt.figure(figsize=(10,5))
+plt.gcf()
+plt.gcf()
+plt.figure(figsize=(15,10))
 plt.bar(yearly['Years'], yearly['Returns'], color=colors, align='center')
 plt.title('Yearly Returns')
 plt.xlabel('Date')
@@ -105,16 +79,5 @@ plt.ylabel('Returns')
 plt.show()
 
 dataset['Returns'] = dataset['Adj Close'].pct_change().dropna()
-
 yearly_returns_avg = dataset['Returns'].groupby([dataset.index.year]).mean()
-
-yearly_returns_avg
-
-colors = yearly.ReturnsPositive.map({True: 'g', False: 'r'})
-plt.figure(figsize=(10,5))
-plt.bar(yearly['Years'], yearly['Returns'], color=colors, align='center')
-plt.plot(yearly_returns_avg, marker='o', color='b')
-plt.title('Yearly Returns')
-plt.xlabel('Date')
-plt.ylabel('Returns')
-plt.show()
+print(yearly_returns_avg)
