b93e7848603fd8327719f0ed2c02e0b41c0937fa
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-08-20 11:28:50 -0400

    Update optimized_bollinger_bands.py

--- Portfolio_Strategies/optimized_bollinger_bands.py
+++ Portfolio_Strategies/optimized_bollinger_bands.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas_datareader.data as pdr
 import yfinance as yf
+import talib as ta
 yf.pdr_override()
+pd.set_option('display.max_columns', None)
 
 def get_data(sym):
     df = pdr.get_data_yahoo(sym)
     df = df.reset_index()
     df["Date"] = pd.to_datetime(df["Date"])
-    # we need to shift or we will have lookahead bias in code
     df["Returns"] = (df["Close"].shift(1) - df["Close"].shift(2)) / df["Close"].shift(2)
     return df
 
@@ -17,7 +18,7 @@ def plot_candles(df, l=0):
     db = df.copy()
     if l > 0:
         db = db[-l:]
-    db = db.reset_index(drop=True).reset_index()
+    db = db.set_index('Date')
     db["Up"] = db["Close"] > db["Open"]
     db["Bottom"] = np.where(db["Up"], db["Open"], db["Close"])
     db["Bar"] = db["High"] - db["Low"]
@@ -26,11 +27,11 @@ def plot_candles(df, l=0):
     fig, ax = plt.subplots(1, 1, figsize=(15, 10))
     plt.title(f'{sym} Optimized Bollinger Bands')
     plt.ylabel('Price')
-    plt.xlabel('Dates')
-    ax.bar(db["index"], bottom=db["Low"], height=db["Bar"], width=0.25, color="#000000")
-    ax.bar(db["index"], bottom=db["Bottom"], height=db["Body"], width=0.5, color=db["Color"])
-    ax.plot(db["OVB"], color="r", linewidth=.25)
-    ax.plot(db["OVS"], color="r", linewidth=.25)
+    plt.xlabel('Date')
+    ax.plot(db["Adj Close"], color="b", linewidth=.3)
+    ax.plot(db['SMA'], color="g", linewidth=.25)
+    ax.plot(db["OVB"], color="r", linewidth=.3)
+    ax.plot(db["OVS"], color="r", linewidth=.3)
     plt.show()
 
 def add_momentum(df, lb=20, std=2):
@@ -38,6 +39,7 @@ def add_momentum(df, lb=20, std=2):
     df["STD"] = df["Returns"].rolling(lb).std()
     df["OVB"] = df["Close"].shift(1) * (1 + (df["MA"] + df["STD"] * std))
     df["OVS"] = df["Close"].shift(1) * (1 + (df["MA"] - df["STD"] * std))
+    df['SMA'] = ta.SMA(df['Adj Close'], timeperiod = 20)
     return df
 
 def stats(df):
@@ -45,18 +47,15 @@ def stats(df):
     ins1 = df[(df["Close"] > df["OVS"]) & (df["Close"] < df["OVB"])]
     ins2 = df[(df["Close"] > df["OVS"])]
     ins3 = df[(df["Close"] < df["OVB"])]
-    il1 = len(ins1)
-    il2 = len(ins2)
-    il3 = len(ins3)
-    r1 = np.round(il1 / total * 100, 2)
-    r2 = np.round(il2 / total * 100, 2)
-    r3 = np.round(il3 / total * 100, 2)
+    r1 = np.round(len(ins1) / total * 100, 2)
+    r2 = np.round(len(ins2) / total * 100, 2)
+    r3 = np.round(len(ins3) / total * 100, 2)
     return r1, r2, r3
 
 sym = input('Enter a ticker: ')
 df = get_data(sym)
-df["Returns"].hist(bins=750, grid=False, figsize=(15,10))
+# df["Returns"].hist(bins=750, grid=False, figsize=(15,10))
 
-df = add_momentum(df, 20, 2)
-plot_candles(df, 100)
+df = add_momentum(df, 20, 3)
+plot_candles(df, 150)
 print(stats(df))
