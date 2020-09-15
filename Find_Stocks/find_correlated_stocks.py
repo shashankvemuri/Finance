ff6e14c3434ce022c6bdbbf9fc1307c8d135e15c
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-09-15 15:07:43 -0400

    updates

--- Find_Stocks/find_correlated_stocks.py
+++ Find_Stocks/find_correlated_stocks.py
@@ -5,22 +5,22 @@ import yfinance as yf
 import datetime as dt
 from yahoo_fin import stock_info as si
 import pandas as pd
+pd.set_option('display.max_rows', None)
 warnings.filterwarnings("ignore")
 yf.pdr_override()
 
-num_of_years = 40
+num_of_years = 1
 start = dt.date.today() - dt.timedelta(days = int(365.25*num_of_years))
 end = dt.date.today()
 
 tickers = si.tickers_dow()
-tickers = [item.replace(".", "-") for item in tickers]
 
 dataset = pdr.get_data_yahoo(tickers, start, end)['Adj Close']
 stocks_returns = np.log(dataset/dataset.shift(1))
 
-print('\nCorrelation Matrix')
+# print('\nCorrelation Matrix')
 corr_matrix = stocks_returns.corr()
-print (corr_matrix)
+# print (corr_matrix)
 
 def get_redundant_pairs(df):
     pairs_to_drop = set()
@@ -37,4 +37,4 @@ def get_top_abs_correlations(df):
     return au_corr
 
 print("\nTop Absolute Correlations")
-print(get_top_abs_correlations(stocks_returns))
+print(get_top_abs_correlations(stocks_returns).head(25))
