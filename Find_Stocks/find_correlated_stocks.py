a9c04e62803c44bd3f9f355e94db1a0755bd805b
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-09-16 20:35:36 -0400

    Update find_correlated_stocks.py

--- Find_Stocks/find_correlated_stocks.py
+++ Find_Stocks/find_correlated_stocks.py
@@ -5,6 +5,7 @@ import yfinance as yf
 import datetime as dt
 from yahoo_fin import stock_info as si
 import pandas as pd
+import matplotlib.pyplot as plt
 pd.set_option('display.max_rows', None)
 warnings.filterwarnings("ignore")
 yf.pdr_override()
@@ -14,13 +15,38 @@ start = dt.date.today() - dt.timedelta(days = int(365.25*num_of_years))
 end = dt.date.today()
 
 tickers = si.tickers_dow()
+tickers = [item.replace('.', '-') for item in tickers]
 
 dataset = pdr.get_data_yahoo(tickers, start, end)['Adj Close']
 stocks_returns = np.log(dataset/dataset.shift(1))
 
-# print('\nCorrelation Matrix')
+print('\nCorrelation Matrix')
 corr_matrix = stocks_returns.corr()
-# print (corr_matrix)
+print (corr_matrix)
+
+def visualize_data(df_corr):
+    data1 = df_corr.values
+    plt.rcParams['figure.figsize'] = (15, 10)
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+
+    heatmap1 = ax1.pcolor(data1, cmap=plt.cm.RdYlGn)
+    fig1.colorbar(heatmap1)
+
+    ax1.set_xticks(np.arange(data1.shape[1]) + 0.5, minor=False)
+    ax1.set_yticks(np.arange(data1.shape[0]) + 0.5, minor=False)
+    ax1.invert_yaxis()
+    ax1.xaxis.tick_top()
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax1.set_xticklabels(column_labels)
+    ax1.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap1.set_clim(-1, 1)
+    plt.tight_layout()
+    plt.show()
+
+visualize_data(corr_matrix)
 
 def get_redundant_pairs(df):
     pairs_to_drop = set()
