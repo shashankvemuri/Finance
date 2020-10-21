69b1ca73febef010ec91f0d738d7091836f1e969
Shashank Vemuri <49424748+shashankvemuri@users.noreply.github.com>
2020-10-21 18:51:20 -0400

    Update finviz_screener.py

--- Find_Stocks/finviz_screener.py
+++ Find_Stocks/finviz_screener.py
@@ -5,14 +5,13 @@ from urllib.request import Request, urlopen
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-# Set up scraper
-url = ("https://finviz.com/screener.ashx?v=151&f=cap_midover,fa_epsyoy1_o20,fa_salesqoq_o20,geo_usa,ind_stocksonly,sh_avgvol_o500,sh_insttrans_pos,sh_price_o10,ta_highlow52w_a30h,ta_perf_52w50o,ta_perf2_13wup,ta_sma20_pa,ta_sma200_pa,ta_sma50_pa&ft=4&o=-volume&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-html = soup(webpage, "html.parser")
-
-def own_screener():
+def long_term_buys_screener():
     try:
+        url = ("https://finviz.com/screener.ashx?v=151&f=an_recom_holdbetter,cap_midover,fa_epsyoy1_o20,fa_salesqoq_o20,geo_usa,ind_stocksonly,sh_avgvol_o500,sh_insttrans_pos,sh_price_o10,ta_highlow52w_a30h,ta_perf_52w50o,ta_perf2_13wup,ta_sma20_pa,ta_sma200_pa,ta_sma50_pa,targetprice_below&ft=4&o=change&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        html = soup(webpage, "html.parser")
+        
         stocks = pd.read_html(str(html))[-2]
         stocks.columns = stocks.iloc[0]
         stocks = stocks[1:]
@@ -21,17 +20,16 @@ def own_screener():
     except Exception as e:
         return e
 
-print ('\nLong Term Buys Screener Output: ')
-print(own_screener())
+print ('\nLong Term Buys Screener: ')
+print(long_term_buys_screener())
 
-# Set up scraper
-url = ("https://finviz.com/screener.ashx?v=151&f=cap_midover,fa_epsyoy1_o20,fa_salesqoq_o20,geo_usa,ind_stocksonly,ipodate_prev3yrs,sh_avgvol_o500,sh_price_o15,ta_changeopen_u,ta_sma20_pa,ta_sma200_pa,ta_sma50_pa&ft=4&o=-volume&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-html = soup(webpage, "html.parser")
-
-def alpha_screener():
+def intraday_buys_screener():
     try:
+        url = ("https://finviz.com/screener.ashx?v=151&f=cap_midover,fa_epsyoy1_o20,fa_salesqoq_o20,geo_usa,ind_stocksonly,ipodate_prev3yrs,sh_avgvol_o500,sh_price_o15,ta_changeopen_u,ta_sma20_pa,ta_sma200_pa,ta_sma50_pa&ft=4&o=-change&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        html = soup(webpage, "html.parser")
+        
         stocks = pd.read_html(str(html))[-2]
         stocks.columns = stocks.iloc[0]
         stocks = stocks[1:]
@@ -41,16 +39,15 @@ def alpha_screener():
         return e
 
 print ('\nIntraday Buys Screener Output: ')
-print(alpha_screener())
+print(intraday_buys_screener())
 
-# Set up scraper
-url = ("https://finviz.com/screener.ashx?v=151&f=cap_smallover,geo_usa,ind_stocksonly,sh_avgvol_o300,sh_insidertrans_neg,sh_price_o5,ta_sma20_pb,ta_sma200_pa100,targetprice_below&ft=4&o=-volume&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-html = soup(webpage, "html.parser")
-
-def long_short_screener():
+def long_term_shorts_screener():
     try:
+        url = ("https://finviz.com/screener.ashx?v=151&f=cap_smallover,geo_usa,ind_stocksonly,sh_avgvol_o300,sh_price_o5,ta_sma20_pb,ta_sma200_pa100,targetprice_above&ft=4&o=-change&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        html = soup(webpage, "html.parser")
+        
         stocks = pd.read_html(str(html))[-2]
         stocks.columns = stocks.iloc[0]
         stocks = stocks[1:]
@@ -60,16 +57,15 @@ def long_short_screener():
         return e
 
 print ('\nLong Term Short Screener Output: ')
-print(long_short_screener())
+print(long_term_shorts_screener())
 
-# Set up scraper
-url = ("https://finviz.com/screener.ashx?v=151&f=cap_smallover,geo_usa,ind_stocksonly,sh_avgvol_o300,sh_insidertrans_neg,sh_insttrans_neg,sh_price_o7,ta_changeopen_d,ta_rsi_ob60,ta_sma200_pa100,targetprice_below&ft=4&o=-volume&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-html = soup(webpage, "html.parser")
-
-def int_short_screener():
+def intraday_shorts_screener():
     try:
+        url = ("https://finviz.com/screener.ashx?v=151&f=cap_smallover,geo_usa,ind_stocksonly,sh_price_o4,ta_change_u10,ta_rsi_ob60,ta_sma200_pa100&ft=4&o=change&ar=180&c=0,1,2,3,4,5,6,7,14,17,18,23,26,27,28,29,42,43,44,45,46,47,48,49,51,52,53,54,57,58,59,60,62,63,64,65,66,67,68,69")
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        html = soup(webpage, "html.parser")
+        
         stocks = pd.read_html(str(html))[-2]
         stocks.columns = stocks.iloc[0]
         stocks = stocks[1:]
@@ -79,4 +75,4 @@ def int_short_screener():
         return e
 
 print ('\nIntraday Short Screener Output: ')
-print(int_short_screener())
+print(intraday_shorts_screener())
