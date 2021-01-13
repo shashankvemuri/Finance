518a90653d765624c5b840491b957a52f01cb12b
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2021-01-12 20:05:12 -0500

    Update finviz_growth.py

--- Find_Stocks/finviz_growth.py
+++ Find_Stocks/finviz_growth.py
@@ -9,7 +9,7 @@ def growth_screener():
     try:
         frames = []
         for i in range(1, 105, 20):
-            url = (f"https://finviz.com/screener.ashx?v=111&f=cap_midover,fa_epsyoy_o20,fa_epsyoy1_o25,fa_roe_o10,ind_stocksonly,sh_avgvol_o100,sh_insttrans_pos,sh_price_o10,ta_sma200_pa,ta_sma50_pa&ft=4&r=0{i}")
+            url = (f"https://finviz.com/screener.ashx?v=151&f=fa_epsqoq_o15,fa_epsyoy_pos,fa_epsyoy1_o25,fa_grossmargin_pos,fa_salesqoq_o25,ind_stocksonly,sh_avgvol_o300,sh_insttrans_pos,sh_price_o10,ta_perf_52w50o,ta_sma200_pa,ta_sma50_pa&ft=4&o=-marketcap&r=0{i}")
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
             webpage = urlopen(req).read()
             html = soup(webpage, "html.parser")
@@ -26,8 +26,10 @@ def growth_screener():
     except Exception as e:
         return e
 
-print ('\nGrowth Stocks Screener: ')
-print(growth_screener())
+df = growth_screener()
+tickers = df.index
 
-for ticker in growth_screener().index:
-    print (ticker+', ')
+print ('\nGrowth Stocks Screener: ')
+print (df)
+print ('\nList of Stocks: : ')
+print (*tickers, sep =', ')
