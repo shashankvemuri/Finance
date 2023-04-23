6360793ce98982faf51866018fefaeda7feac8f7
shashankvemuri <shashank.vemuri1@gmail.com>
2023-04-23 16:02:10 -0400

    Update correlated_stocks.py

--- Find_Stocks/correlated_stocks.py
+++ Find_Stocks/correlated_stocks.py
@@ -1,32 +1,32 @@
-import numpy as np
-import warnings
-from pandas_datareader import data as pdr
-import yfinance as yf
+# Import dependencies
 import datetime as dt
-from yahoo_fin import stock_info as si
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-pd.set_option('display.max_rows', None)
-warnings.filterwarnings("ignore")
-yf.pdr_override()
+from pandas_datareader import data as pdr
+from yahoo_fin import stock_info as si
+import yfinance as yf
 
+# Set up date range and get stock tickers
 num_of_years = 1
-start = dt.date.today() - dt.timedelta(days = int(365.25*num_of_years))
+start = dt.date.today() - dt.timedelta(days=int(365.25 * num_of_years))
 end = dt.date.today()
-
 tickers = si.tickers_dow()
-tickers = [item.replace('.', '-') for item in tickers]
+tickers = [item.replace(".", "-") for item in tickers]
 
-dataset = pdr.get_data_yahoo(tickers, start, end)['Adj Close']
-stocks_returns = np.log(dataset/dataset.shift(1))
+# Get daily adjusted close prices for all tickers
+dataset = pdr.get_data_yahoo(tickers, start, end)["Adj Close"]
+stocks_returns = np.log(dataset / dataset.shift(1))
 
-print('\nCorrelation Matrix')
+# Compute correlation matrix and display it
+print("\nCorrelation Matrix")
 corr_matrix = stocks_returns.corr()
-print (corr_matrix)
+print(corr_matrix)
 
-def visualize_data(df_corr):
+# Visualize correlation matrix as heatmap
+def visualize_correlation_matrix(df_corr):
     data1 = df_corr.values
-    plt.rcParams['figure.figsize'] = (15, 10)
+    plt.rcParams["figure.figsize"] = (15, 10)
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
 
@@ -46,21 +46,23 @@ def visualize_data(df_corr):
     plt.tight_layout()
     plt.show()
 
-visualize_data(corr_matrix)
+visualize_correlation_matrix(corr_matrix)
 
+# Define helper functions for finding top correlated pairs
 def get_redundant_pairs(df):
     pairs_to_drop = set()
     cols = df.columns
     for i in range(0, df.shape[1]):
-        for j in range(0, i+1):
+        for j in range(0, i + 1):
             pairs_to_drop.add((cols[i], cols[j]))
     return pairs_to_drop
 
-def get_top_abs_correlations(df):
+def get_top_abs_correlations(df, n=25):
     au_corr = df.corr().abs().unstack()
     labels_to_drop = get_redundant_pairs(df)
     au_corr = au_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
-    return au_corr
+    return au_corr[0:n]
 
+# Display top absolute correlations
 print("\nTop Absolute Correlations")
-print(get_top_abs_correlations(stocks_returns).head(25))
+print(get_top_abs_correlations(stocks_returns)) 
