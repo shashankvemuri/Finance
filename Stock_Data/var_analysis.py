95f743afd1d5a95c37c8cb0962481c86a9c4de91
shashankvemuri <shashank.vemuri1@gmail.com>
2023-04-24 16:09:16 -0400

    value at risk

--- Stock_Data/var_analysis.py
+++ Stock_Data/var_analysis.py
@@ -1,42 +1,47 @@
+# Import dependencies
 import numpy
 import scipy.stats
 import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings("ignore")
 from pandas_datareader import data as pdr
 import yfinance as yf
-yf.pdr_override()
 import datetime as dt
 
+# Set stock and date range for data retrieval
 stock = "AMD"
-start = dt.date.today() - dt.timedelta(days = 365*9)
+start = dt.date.today() - dt.timedelta(days=365*9)
 end = dt.date.today()
+
+# Retrieve data from Yahoo Finance API
 df = pdr.get_data_yahoo(stock, start, end)
 
+# Plot adjusted close prices
 plt.figure(figsize=(15,8))
 df["Adj Close"].plot()
 plt.title(f"{stock} Adj Close", weight='bold')
 plt.show()
 
+# Plot daily percentage returns
 plt.gcf()
 fig = plt.figure()
 fig.set_size_inches(10,3)
 df["Adj Close"].pct_change().plot()
-plt.title(u"{} daily returns".format(stock), weight='bold')
+plt.title("{} daily returns".format(stock), weight='bold')
 plt.show()
 
+# Plot histogram of daily returns
 plt.gcf()
 df["Adj Close"].pct_change().hist(bins=50, density=True, histtype='stepfilled', alpha=0.5)
-plt.title(u"Histogram of stock daily returns", weight='bold')
-df["Adj Close"].pct_change().std()
+plt.title("Histogram of stock daily returns", weight='bold')
 plt.show()
 
+# Plot QQ plot of daily returns
 Q = df["Adj Close"].pct_change().dropna().tolist()
 plt.gcf()
 scipy.stats.probplot(Q, dist=scipy.stats.norm, plot=plt.figure().add_subplot(111))
 plt.title("Normal QQ-plot of daily returns", weight="bold");
 plt.show()
 
+# Plot student QQ plot of daily returns
 tdf, tmean, tsigma = scipy.stats.t.fit(Q)
 plt.gcf()
 scipy.stats.probplot(Q, dist=scipy.stats.t, sparams=(tdf, tmean, tsigma), plot=plt.figure().add_subplot(111))
@@ -48,7 +53,6 @@ returns = df["Adj Close"].pct_change().dropna()
 mean = returns.mean()
 sigma = returns.std()
 plt.gcf()
-tdf, tmean, tsigma = scipy.stats.t.fit(returns.tolist())
 returns.hist(bins=40, density=True, histtype='stepfilled', alpha=0.5);
 plt.show()
 
@@ -57,10 +61,13 @@ support = numpy.linspace(returns.min(), returns.max(), 100)
 plt.gcf()
 returns.hist(bins=40, density=True, histtype='stepfilled', alpha=0.5);
 plt.plot(support, scipy.stats.t.pdf(support, loc=tmean, scale=tsigma, df=tdf), "r-")
-plt.title("Daily change in HAL (%)", weight='bold')
+plt.title("VaR using variance-covariance method", weight='bold')
 plt.show()
 
-print(scipy.stats.norm.ppf(0.05, mean, sigma))
+# Calculate VaR using the normal distribution and 95% confidence level
+VaR = scipy.stats.norm.ppf(0.05, mean, sigma)
+print("VaR using normal distribution and 95% confidence level: ", VaR)
+
 # VaR using Monte Carlo method
 days = 300   # time horizon
 dt = 1/float(days)
@@ -68,6 +75,9 @@ sigma = 0.04 # volatility
 mu = 0.05  # drift (average growth rate)
 
 def random_walk(startprice):
+    """
+    Simulates a random walk for a given number of days
+    """
     price = numpy.zeros(days)
     shock = numpy.zeros(days)
     price[0] = startprice
@@ -76,12 +86,12 @@ def random_walk(startprice):
         price[i] = max(0, price[i-1] + shock[i] * price[i-1])
     return price
 
-# Similuations
+# Simulations
 plt.gcf()
 for run in range(30):
     plt.plot(random_walk(10.0))
 plt.xlabel("Time")
-plt.ylabel("Price");
+plt.ylabel("Price")
 plt.show()
 
 runs = 10000
