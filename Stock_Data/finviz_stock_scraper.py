b050d20e7685c915c445d1b0516ce6fdff84a238
shashankvemuri <shashank.vemuri1@gmail.com>
2023-04-24 16:12:09 -0400

    finviz stock data

--- Stock_Data/finviz_stock_scraper.py
+++ Stock_Data/finviz_stock_scraper.py
@@ -1,7 +1,9 @@
+# Import dependencies
 import pandas as pd
 import json
 import finviz
 
+# Set display options for pandas dataframes
 pd.set_option('display.max_colwidth', 25)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -10,9 +12,15 @@ pd.set_option('display.max_rows', None)
 symbol = input('Enter a ticker: ')
 
 def get_fundamentals(ticker):
+    """
+    Function to get fundamental ratios for a given stock ticker
+    """
     try:
+        # Get data from finviz and convert to pandas dataframe
         fundamentals = json.dumps(finviz.get_stock(ticker))
         fundamentals = pd.read_json(fundamentals, orient='index')
+        
+        # Set column names and index, and return the resulting dataframe
         fundamentals = fundamentals.reset_index()
         fundamentals.columns = ['Attributes', 'Values']
         fundamentals = fundamentals.set_index('Attributes')
@@ -21,7 +29,11 @@ def get_fundamentals(ticker):
         return e
     
 def get_news(ticker):
+    """
+    Function to get recent news articles for a given stock ticker
+    """
     try:
+        # Get news from finviz and convert to pandas dataframe
         news = finviz.get_news(ticker)
         news = pd.DataFrame(news, columns=['Headline', 'Link'])
         news = news.set_index('Headline')
@@ -30,7 +42,11 @@ def get_news(ticker):
         return e
 
 def get_insider(ticker):
+    """
+    Function to get recent insider trades for a given stock ticker
+    """
     try:
+        # Get insider trading data from finviz and convert to pandas dataframe
         insider = finviz.get_insider(ticker)
         insider = pd.DataFrame(insider)
         insider = insider.set_index('Date')
@@ -39,7 +55,11 @@ def get_insider(ticker):
         return e
 
 def get_price_targets(ticker):
+    """
+    Function to get analyst price targets for a given stock ticker
+    """
     try:
+        # Get price target data from finviz and convert to pandas dataframe
         targets = finviz.get_analyst_price_targets(ticker)
         targets = pd.DataFrame(targets)
         targets.columns = ['Date', 'Category', 'Analyst', 'Rating', 'Price From', 'Price To']
@@ -48,14 +68,15 @@ def get_price_targets(ticker):
     except Exception as e:
         return e
 
-print ('Fundamental Ratios: ')
+# Print out the resulting dataframes for each category
+print('Fundamental Ratios:')
 print(get_fundamentals(symbol))
 
-print ('\nRecent News: ')
+print('\nRecent News:')
 print(get_news(symbol))
 
-print ('\nRecent Insider Trades: ')
+print('\nRecent Insider Trades:')
 print(get_insider(symbol))
 
-print ('\nAnalyst Price Targets: ')
+print('\nAnalyst Price Targets:')
 print(get_price_targets(symbol))
