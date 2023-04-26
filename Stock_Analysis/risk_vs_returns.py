2eb51a7f071ce94e2d73951e471ec0ea7ca478a7
shashankvemuri <shashank.vemuri1@gmail.com>
2023-04-26 01:02:23 -0400

    Update risk_vs_returns.py

--- Stock_Analysis/risk_vs_returns.py
+++ Stock_Analysis/risk_vs_returns.py
@@ -2,79 +2,78 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-yf.pdr_override()
 import datetime as dt
-from IPython.display import Latex
 
-# input
+# Set up the input variables
 symbols = ['AAPL', 'MSFT', 'AMD', 'INTC', 'NVDA']
-start = dt.date.today() - dt.timedelta(days = 365*3)
+start = dt.date.today() - dt.timedelta(days=365*3)
 end = dt.date.today()
 
+# Create an empty DataFrame
 df = pd.DataFrame()
 data = []
+
+# Loop through each stock ticker, download the stock data, and add it to the DataFrame
 for symbol in symbols:
-    df = pd.merge(df, pd.DataFrame(yf.download(symbol, fields='price', 
-                           start=start, end=end)['Adj Close']), 
-                           right_index=True, left_index=True, how='outer')
-       
-    # Appends tickers which have data
+    # Download the stock data
+    stock_data = yf.download(symbol, fields='price', start=start, end=end)
+
+    # Extract the adjusted closing prices and merge with the main DataFrame
+    adj_close = pd.DataFrame(stock_data['Adj Close'])
+    df = pd.merge(df, adj_close, right_index=True, left_index=True, how='outer')
+
+    # Append the stock ticker to the list of tickers with available data
     data.append(symbol)
 
+# Set the column names of the DataFrame to the tickers with available data
 df.columns = data
+
+# Drop any columns that have missing data
 df = df.dropna(axis='columns')
 
-Latex(r"""\begin{eqnarray}r_t = \frac{AdjClose_t - AdjClose_{t-1}}{AdjClose_{t-1}} = \frac{AdjClose_t}{AdjClose_{t-1}} - 1
-\end{eqnarray}""")
-
-from pandas.plotting import scatter_matrix
+# Calculate the percentage change in stock prices over the past 3 periods
 rets = df.pct_change(periods=3)
+
+# Create a scatter plot matrix of the percentage changes in stock prices
+from pandas.plotting import scatter_matrix
 scatter_matrix(rets, diagonal='kde', figsize=(10, 10))
+
+# Calculate the correlation matrix of the percentage changes in stock prices
 corr = rets.corr()
+
+# Create a heatmap of the correlation matrix
 plt.imshow(corr, cmap='Blues', interpolation='none')
 plt.colorbar()
 plt.xticks(range(len(corr)), corr.columns)
 plt.yticks(range(len(corr)), corr.columns)
 
+# Create a bar chart of the standard deviations of the percentage changes in stock prices
 plt.bar(rets.columns, rets.std(), color=['red', 'blue', 'green', 'orange', 'cyan'])
 plt.title("Stock Risk")
 plt.xlabel("Stock Symbols")
 plt.ylabel("Standard Deviations")
-plt.subplots()
-plt.show()
 
+# Create a bar chart of the average percentage changes in stock prices
 plt.bar(rets.columns, rets.mean(), color=['red', 'blue', 'green', 'orange', 'cyan'])
 plt.title("Average Returns")
 plt.xlabel("Stock Symbols")
 plt.ylabel("Returns")
-plt.subplots()
-plt.show()
 
+# Create a bar chart comparing the average percentage changes in stock prices to their standard deviations
 ind = np.arange(5)
 width = 0.35       
-plt.bar(ind, rets.mean(), width, color = 'g', label='Average of Returns')
-plt.bar(ind + width, rets.std(), width, color = 'r', label='Risk of Returns')
+plt.bar(ind, rets.mean(), width, color='g', label='Average of Returns')
+plt.bar(ind + width, rets.std(), width, color='r', label='Risk of Returns')
 plt.ylabel('Returns Scores')
 plt.xlabel('Symbols')
 plt.title('Risk vs Return')
-plt.subplots()
-plt.show()
-
 plt.xticks(ind + width / 2, ('AAPL', 'MSFT', 'AMD', 'INTC', 'NVDA'))
 plt.legend(loc='best')
-plt.subplots()
-plt.show()
 
+# Create stacked bar charts comparing the average percentage changes in stock prices to their standard deviations
 ind = [x for x, _ in enumerate(symbols)]
-
 plt.bar(ind, rets.mean(), width=0.8, label='Average of Returns', color='b')
-plt.subplots()
-plt.show()
-
 plt.bar(ind, rets.std(), width=0.8, label='Risk of Returns', color='r', bottom=rets.mean())
-plt.subplots()
-plt.show()
-
 plt.xticks(ind, symbols)
 plt.ylabel("Returns Score")
 plt.xlabel("Symbols")
@@ -83,7 +82,7 @@ plt.title('Risk vs Return')
 plt.subplots()
 plt.show()
 
-# ## Risk vs Expected Return
+# Create scatter plot to show expected returns vs risk
 plt.scatter(rets.mean(), rets.std())
 plt.xlabel('Expected returns')
 plt.ylabel('Risk')
@@ -98,6 +97,7 @@ for label, x, y in zip(rets.columns, rets.mean(), rets.std()):
 plt.subplots()
 plt.show()
 
+# Display table with risk vs expected returns
 d = {'Risk':rets.std(), 'Expected Returns':rets.mean()}
 print('Table: Risk vs Expected Returns')
 tables = pd.DataFrame(data=d)
