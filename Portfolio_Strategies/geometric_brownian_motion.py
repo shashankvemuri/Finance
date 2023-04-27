cb6cc5eec44bf0b85869bcaa4549e1bf92a67b8d
shashankvemuri <shashank.vemuri1@gmail.com>
2023-04-27 15:11:17 -0400

    Update geometric_brownian_motion.py

--- Portfolio_Strategies/geometric_brownian_motion.py
+++ Portfolio_Strategies/geometric_brownian_motion.py
@@ -1,3 +1,4 @@
+# Import dependencies
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -6,38 +7,39 @@ from pandas_datareader import DataReader
 import pandas as pd
 import datetime
 from pylab import rcParams
-from yahoo_fin import stock_info as si
 
+# Define the number of years to go back in time to fetch data
 num_of_years = 5
-start_date = datetime.datetime.now() - datetime.timedelta(days=int(355.25*5))
-end_date = datetime.date.today()
 
+# Set the start and end dates for fetching data
+end_date = datetime.date.today()
+start_date = end_date - datetime.timedelta(days=int(355.25*num_of_years))
+
+# Set the stock and index symbols to fetch data for
 stock = 'NIO'
 index = '^GSPC'
 
-df = DataReader(stock,'yahoo', start_date, end_date)
-dfb = DataReader(index,'yahoo', start_date, end_date)
+# Fetch stock and index data from Yahoo Finance
+df = DataReader(stock, 'yahoo', start_date, end_date)
+dfb = DataReader(index, 'yahoo', start_date, end_date)
 
+# Resample the data to monthly frequency
 rts = df.resample('M').last()
 rbts = dfb.resample('M').last()
-dfsm = pd.DataFrame({'s_adjclose' : rts['Adj Close'],
-                        'b_adjclose' : rbts['Adj Close']},
-                        index=rts.index)
 
-dfsm[['s_returns','b_returns']] = dfsm[['s_adjclose','b_adjclose']]/\
-    dfsm[['s_adjclose','b_adjclose']].shift(1) -1
+# Create a dataframe with adjusted close prices for the stock and index
+dfsm = pd.DataFrame({'s_adjclose': rts['Adj Close'], 'b_adjclose': rbts['Adj Close']}, index=rts.index)
+
+# Calculate the monthly returns for the stock and index
+dfsm[['s_returns', 'b_returns']] = dfsm[['s_adjclose', 'b_adjclose']] / dfsm[['s_adjclose', 'b_adjclose']].shift(1) - 1
+
+# Drop rows with missing data
 dfsm = dfsm.dropna()
-covmat = np.cov(dfsm["s_returns"],dfsm["b_returns"])
+
+# Calculate the covariance matrix of the monthly returns
+covmat = np.cov(dfsm['s_returns'], dfsm['b_returns'])
 
 class GBM:
-
-    def simulate(self):
-        while(self.total_time > 0):
-            dS = self.current_price*self.drift*self.time_period + self.current_price*self.volatility*np.random.normal(0, math.sqrt(self.time_period))
-            self.prices.append(self.current_price + dS)
-            self.current_price += dS
-            self.total_time -= self.time_period
-
     def __init__(self, initial_price, drift, volatility, time_period, total_time):
         # Initialize fields
         self.initial_price = initial_price
@@ -48,23 +50,35 @@ class GBM:
         self.total_time = total_time
         self.prices = []
         # Simulate the diffusion process
-        self.simulate()   # Simulate the diffusion proces
+        self.simulate()
 
-simulations = []
-n = 20
-initial_price = si.get_live_price(stock)
-drift = .24
-volatility = np.sqrt(covmat[0,0])
-time_period = 1/365 # Daily
-total_time = 1
+    def simulate(self):
+        while self.total_time > 0:
+            # Calculate the change in the stock price using the Geometric Brownian Motion formula
+            dS = self.current_price * self.drift * self.time_period + \
+                self.current_price * self.volatility * np.random.normal(0, math.sqrt(self.time_period))
+            # Update the stock price
+            self.prices.append(self.current_price + dS)
+            self.current_price += dS
+            self.total_time -= self.time_period
 
-for i in range(0, n):
-    simulations.append(GBM(initial_price, drift, volatility, time_period, total_time))
+# Set the parameters for the Geometric Brownian Motion simulation
+n = 20 # Number of simulations
+initial_price = si.get_live_price(stock) # Use the live stock price as the initial price
+drift = .24 # Use a constant drift value
+volatility = np.sqrt(covmat[0,0]) # Use the volatility of the stock
+time_period = 1 / 365 # Daily
+total_time = 1 # 1 day
 
+# Run the Geometric Brownian Motion simulation n times
+simulations = [GBM(initial_price, drift, volatility, time_period, total_time) for i in range(n)]
+
+# Plot the results
 rcParams['figure.figsize'] = 15, 10 
 for sim in simulations:
     plt.plot(np.arange(0, len(sim.prices)), sim.prices)
 
+# Add the title, legend, and axis labels
 plt.title(f'Geometric Brownian Motion for {stock.upper()}')
 plt.axhline(y=si.get_live_price(stock), color='r')
 plt.xlabel('Time')
