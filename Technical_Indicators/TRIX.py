6a0f99a8ec96a39b90a30cc208756cce295977ca
shashankvemuri <shashank.vemuri1@gmail.com>
2023-04-28 14:06:07 -0400

    formatting

--- Technical_Indicators/TRIX.py
+++ Technical_Indicators/TRIX.py
@@ -1,70 +1,73 @@
+# Import dependencies
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-yf.pdr_override()
 import datetime as dt
+yf.pdr_override()
 
 # input
-symbol = 'AAPL'
-start = dt.date.today() - dt.timedelta(days = 180)
+symbol = "AAPL"
+start = dt.date.today() - dt.timedelta(days=180)
 end = dt.date.today()
 
-# Read data 
-df = yf.download(symbol,start,end)
+# Read data
+df = yf.download(symbol, start, end)
 
-n = 15 # Number of days
-df['SS_EMA'] = df['Adj Close'].rolling(n).mean()
-df['DS_EMA'] = df['SS_EMA'].rolling(n).mean()
-df['TS_EMA'] = df['DS_EMA'].rolling(n).mean()
-df['TRIX'] = 100*(df['TS_EMA'] - df['TS_EMA'].shift())/df['TS_EMA'].shift()
-df = df.drop(['SS_EMA','DS_EMA','TS_EMA'],axis=1)
+n = 15  # Number of days
+df["SS_EMA"] = df["Adj Close"].rolling(n).mean()
+df["DS_EMA"] = df["SS_EMA"].rolling(n).mean()
+df["TS_EMA"] = df["DS_EMA"].rolling(n).mean()
+df["TRIX"] = 100 * (df["TS_EMA"] - df["TS_EMA"].shift()) / df["TS_EMA"].shift()
+df = df.drop(["SS_EMA", "DS_EMA", "TS_EMA"], axis=1)
 
-fig = plt.figure(figsize=(14,7))
+fig = plt.figure(figsize=(14, 7))
 ax1 = plt.subplot(2, 1, 1)
-ax1.plot(df['Adj Close'])
-ax1.set_title('Stock '+ symbol +' Closing Price')
-ax1.set_ylabel('Price')
-ax1.legend(loc='best')
+ax1.plot(df["Adj Close"])
+ax1.set_title("Stock " + symbol + " Closing Price")
+ax1.set_ylabel("Price")
+ax1.legend(loc="best")
 
 ax2 = plt.subplot(2, 1, 2)
-ax2.plot(df['TRIX'], label='TRIX')
-ax2.axhline(y=0, color='darkblue')
+ax2.plot(df["TRIX"], label="TRIX")
+ax2.axhline(y=0, color="darkblue")
 ax2.grid()
-ax2.legend(loc='best')
-ax2.set_ylabel('TRIX')
-ax2.set_xlabel('Date')
+ax2.legend(loc="best")
+ax2.set_ylabel("TRIX")
+ax2.set_xlabel("Date")
 plt.show()
 
 # ## Candlestick with TRIX
 from matplotlib import dates as mdates
+
 dfc = df.copy()
-dfc['VolumePositive'] = dfc['Open'] < dfc['Adj Close']
-#dfc = dfc.dropna()
+dfc["VolumePositive"] = dfc["Open"] < dfc["Adj Close"]
+# dfc = dfc.dropna()
 dfc = dfc.reset_index()
-dfc['Date'] = mdates.date2num(dfc['Date'].tolist())
+dfc["Date"] = mdates.date2num(dfc["Date"].tolist())
 
 from mplfinance.original_flavor import candlestick_ohlc
-fig = plt.figure(figsize=(14,7))
+
+fig = plt.figure(figsize=(14, 7))
 ax1 = plt.subplot(2, 1, 1)
-candlestick_ohlc(ax1,dfc.values, width=0.5, colorup='g', colordown='r', alpha=1.0)
+candlestick_ohlc(ax1, dfc.values, width=0.5, colorup="g", colordown="r", alpha=1.0)
 ax1.xaxis_date()
-ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
-ax1.grid(True, which='both')
+ax1.xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%Y"))
+ax1.grid(True, which="both")
 ax1.minorticks_on()
 ax1v = ax1.twinx()
-colors = dfc.VolumePositive.map({True: 'g', False: 'r'})
-ax1v.bar(dfc.Date, dfc['Volume'], color=colors, alpha=0.4)
+colors = dfc.VolumePositive.map({True: "g", False: "r"})
+ax1v.bar(dfc.Date, dfc["Volume"], color=colors, alpha=0.4)
 ax1v.axes.yaxis.set_ticklabels([])
-ax1v.set_ylim(0, 3*df.Volume.max())
-ax1.set_title('Stock '+ symbol +' Closing Price')
-ax1.set_ylabel('Price')
+ax1v.set_ylim(0, 3 * df.Volume.max())
+ax1.set_title("Stock " + symbol + " Closing Price")
+ax1.set_ylabel("Price")
 
 ax2 = plt.subplot(2, 1, 2)
-ax2.plot(df['TRIX'], label='TRIX')
-ax2.axhline(y=0, color='darkblue')
+ax2.plot(df["TRIX"], label="TRIX")
+ax2.axhline(y=0, color="darkblue")
 ax2.grid()
-ax2.legend(loc='best')
-ax2.set_ylabel('TRIX')
-ax2.set_xlabel('Date')
+ax2.legend(loc="best")
+ax2.set_ylabel("TRIX")
+ax2.set_xlabel("Date")
 plt.show()
