ab95a74ef654cffc567319ee7a22b8cc335c7085
shashankvemuri <shashank.vemuri1@gmail.com>
2023-04-28 12:30:54 -0400

    probabilistic analysis

--- Machine_Learning/stock_probabilistic_analysis.py
+++ Machine_Learning/stock_probabilistic_analysis.py
@@ -1,19 +1,28 @@
+# Import dependencies
 import yfinance
 from scipy import stats
 import numpy as np
 
+# Download historical data for AMD stock from Yahoo Finance API
 data = yfinance.download('AMD','2015-09-08','2020-09-08')
 
 def calculate_prereq(values):
+    # Calculate standard deviation and mean for a given list of values
     std = np.std(values)
     mean = np.mean(values)
     return std,mean
+
 def calculate_distribution(mean,std):
+    # Create a normal distribution object with a given mean and standard deviation
     norm = stats.norm(mean, std)
     return norm
+
 def extrapolate(norm,x):
+    # Calculate the probability density function for a given normal distribution object and value
     return norm.pdf(x)
+
 def values_to_norm(dicts):
+    # Convert lists of values in a list of dictionaries to normal distribution objects
     for dictionary in dicts:
         for term in dictionary:
             std,mean = calculate_prereq(dictionary[term])
@@ -22,6 +31,7 @@ def values_to_norm(dicts):
     return dicts
 
 def compare_possibilities(dicts,x):
+    # Compare two sets of normal distribution objects for a given list of values and return the index of the set with higher probability
     probabilities = []
     for dictionary in dicts:
         dict_probs = []
@@ -31,6 +41,7 @@ def compare_possibilities(dicts,x):
         probabilities.append(np.prod(dict_probs))
     return probabilities.index(max(probabilities))
 
+# Calculate daily stock ratios and store them in dictionaries based on whether the stock price increased or decreased
 drop = {}
 increase = {}
 for day in range(10,len(data)-1):
@@ -50,7 +61,8 @@ for day in range(10,len(data)-1):
                 drop[i] += (ratios[i],)
             else:
                 drop[i] = ()
-                
+
+# Calculate new daily ratios and store them in the corresponding dictionary
 new_close = data['Close'][-11:-1]
 ratios = []
 for i in range(1,len(new_close)):
@@ -60,9 +72,9 @@ for i in range(len(ratios)):
         increase[i] += (ratios[i],)
     else:
         increase[i] = ()
-            
+
+# Convert lists of ratios in both dictionaries to normal distribution objects and compare the two dictionaries to predict future stock price movements
 X = ratios
-print(X)
 dicts = [increase,drop]
 dicts = values_to_norm(dicts)
 print(compare_possibilities(dicts,X))
