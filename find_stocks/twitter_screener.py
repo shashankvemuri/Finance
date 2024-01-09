b5aafaea650b5a924436bb1a086e5c458263e9a8
shashankvemuri <shashank.vemuri1@gmail.com>
2024-01-09 02:58:12 -0500

    Update twitter_screener.py

--- find_stocks/twitter_screener.py
+++ find_stocks/twitter_screener.py
@@ -1,131 +1,77 @@
 # Import dependencies
-import requests 
-import numpy as np
+import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from alpha_vantage.sectorperformance import SectorPerformances
 
-# Set option to display all columns in dataframes
+# Set pandas option to display all columns
 pd.set_option('display.max_columns', None)
 
-# Get most active stocks for the day
+# Function to scrape most active stocks from Yahoo Finance
 def scrape_most_active_stocks():
     url = 'https://finance.yahoo.com/most-active/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
     df = pd.read_html(str(soup), attrs={'class': 'W(100%)'})[0]
     df = df.drop(columns=['52 Week High'])
     return df
 
-# Usage
+# Scrape and filter most active stocks
 movers = scrape_most_active_stocks()
-
-# Keep only stocks with positive percentage change
 movers = movers[movers['% Change'] >= 0]
 
-# Scrape sentiment data from Sentdex website
-res = requests.get('http://www.sentdex.com/financial-analysis/?tf=30d')
-soup = BeautifulSoup(res.text, features="lxml")
-table = soup.find_all('tr')
+# Scrape sentiment data from Sentdex
+def scrape_sentdex():
+    res = requests.get('http://www.sentdex.com/financial-analysis/?tf=30d')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    table = soup.find_all('tr')
+    data = {'Symbol': [], 'Sentiment': [], 'Direction': [], 'Mentions': []}
 
-stock = []
-sentiment = []
-mentions = []
-sentiment_trend = []
+    for ticker in table:
+        ticker_info = ticker.find_all('td')
+        try:
+            data['Symbol'].append(ticker_info[0].get_text())
+            data['Sentiment'].append(ticker_info[3].get_text())
+            data['Mentions'].append(ticker_info[2].get_text())
+            trend = 'up' if ticker_info[4].find('span', {"class": "glyphicon glyphicon-chevron-up"}) else 'down'
+            data['Direction'].append(trend)
+        except:
+            continue
+    
+    return pd.DataFrame(data)
 
-for ticker in table:
-    ticker_info = ticker.find_all('td')
-    try:
-        stock.append(ticker_info[0].get_text())
-    except:
-        stock.append(None)
-    try:
-        sentiment.append(ticker_info[3].get_text())
-    except:
-        sentiment.append(None)
-    try:
-        mentions.append(ticker_info[2].get_text())
-    except:
-        mentions.append(None)
-    try:
-        if (ticker_info[4].find('span',{"class":"glyphicon glyphicon-chevron-up"})):
-            sentiment_trend.append('up')
-        else:
-            sentiment_trend.append('down')
-    except:
-        sentiment_trend.append(None)
+sentdex_data = scrape_sentdex()
 
-# Create a dataframe with stock sentiment data
-company_info = pd.DataFrame(data={'Symbol': stock, 'Sentiment': sentiment, 'Direction': sentiment_trend, 'Mentions':mentions})
+# Merge most active stocks with sentiment data
+top_stocks = movers.merge(sentdex_data, on='Symbol', how='left')
+top_stocks.drop(['Market Cap', 'PE Ratio (TTM)'], axis=1, inplace=True)
 
-# Merge most active stocks data with sentiment data
-top_stocks = movers.merge(company_info, on='Symbol', how='left')
+# Function to scrape Twitter data from Trade Followers
+def scrape_twitter(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    stock_twitter = soup.find_all('tr')
+    data = {'Symbol': [], 'Sector': [], 'Score': []}
 
-# Drop unnecessary columns from the merged dataframe
-top_stocks.drop(['Market Cap','PE Ratio (TTM)'], axis=1, inplace=True)
+    for stock in stock_twitter:
+        try:
+            score = stock.find_all("td", {"class": "datalistcolumn"})
+            data['Symbol'].append(score[0].get_text().replace('$', '').strip())
+            data['Sector'].append(score[2].get_text().strip())
+            data['Score'].append(score[4].get_text().strip())
+        except:
+            continue
+    
+    return pd.DataFrame(data).dropna().drop_duplicates(subset="Symbol").reset_index(drop=True)
 
-# Scrape Twitter data from Trade Followers website
-res = requests.get("https://www.tradefollowers.com/strength/twitter_strongest.jsp?tf=1m")
-soup = BeautifulSoup(res.text, features="lxml")
-stock_twitter = soup.find_all('tr')
+# Scrape Twitter data and merge with previous data
+twitter_data = scrape_twitter("https://www.tradefollowers.com/strength/twitter_strongest.jsp?tf=1m")
+final_list = top_stocks.merge(twitter_data, on='Symbol', how='left')
 
-twit_stock = []
-sector = []
-twit_score = []
+# Further scrape and merge Twitter data
+twitter_data2 = scrape_twitter("https://www.tradefollowers.com/active/twitter_active.jsp?tf=1m")
+recommender_list = final_list.merge(twitter_data2, on='Symbol', how='left')
+recommender_list.drop(['Volume', 'Avg Vol (3 month)'], axis=1, inplace=True)
 
-for stock in stock_twitter:
-    try:
-        score = stock.find_all("td",{"class": "datalistcolumn"})
-        twit_stock.append(score[0].get_text().replace('$','').strip())
-        sector.append(score[2].get_text().replace('\n','').strip())
-        twit_score.append(score[4].get_text().replace('\n','').strip())
-    except:
-        twit_stock.append(np.nan)
-        sector.append(np.nan)
-        twit_score.append(np.nan)
-
-# Create a dataframe with Twitter data
-twitter_df = pd.DataFrame({'Symbol': twit_stock, 'Sector': sector, 'Twit_Bull_score': twit_score})
-
-# Remove NA values and duplicates from the dataframe
-twitter_df.dropna(inplace=True)
-twitter_df.drop_duplicates(subset ="Symbol", keep='first', inplace=True)
-twitter_df.reset_index(drop=True,inplace=True)
-
-# Merge the previous dataframe with the Twitter momentum data
-Final_list = top_stocks.merge(twitter_df, on='Symbol', how='left')
-
-# Scrape more Twitter data from Trade Followers website
-res2 = requests.get("https://www.tradefollowers.com/active/twitter_active.jsp?tf=1m")
-soup2 = BeautifulSoup(res2.text, features="lxml")
-stock_twitter2 = soup2.find_all('tr')
-
-twit_stock2 = []
-sector2 = []
-twit_score2 = []
-
-for stock in stock_twitter2:
-    try:
-        score2 = stock.find_all("td",{"class": "datalistcolumn"})
-        twit_stock2.append(score2[0].get_text().replace('$','').strip())
-        sector2.append(score2[2].get_text().replace('\n','').strip())
-        twit_score2.append(score2[4].get_text().replace('\n','').strip())
-    except:
-        twit_stock2.append(np.nan)
-        sector2.append(np.nan)
-        twit_score2.append(np.nan)
-        
-twitter_df2 = pd.DataFrame({'Symbol': twit_stock2, 'Sector': sector2, 'Twit_mom': twit_score2})
-
-# Remove NA values 
-twitter_df2.dropna(inplace=True)
-twitter_df2.drop_duplicates(subset ="Symbol", 
-                     keep = 'first', inplace = True)
-twitter_df2.reset_index(drop=True,inplace=True)
-
-# Create and print list
-recommender_list = Final_list.merge(twitter_df2, on='Symbol', how='left')
-recommender_list.drop(['Volume','Avg Vol (3 month)'],axis=1, inplace=True)
+# Print final recommended list
 print('\nFinal Recommended List: ')
 print(recommender_list.set_index('Symbol'))
