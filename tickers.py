4275f665e2625625d4527e910c9e4f9208eb4432
shashankvemuri <shashank.vemuri1@gmail.com>
2024-01-08 20:23:04 -0500

    Update tickers.py

--- tickers.py
+++ tickers.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import requests
 
-def get_sp500_tickers():
+def tickers_sp500():
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     html = requests.get(url).text
     df = pd.read_html(html, header=0)[0]
     tickers = df['Symbol'].tolist()
     return tickers
 
-def get_nasdaq_tickers():
+def tickers_nasdaq():
     url = 'http://www.nasdaqtrader.com/dynamic/symdir/nasdaqlisted.txt'
     data = requests.get(url).text
     # The data is '|' separated and last two lines are not needed
@@ -17,7 +17,7 @@ def get_nasdaq_tickers():
     df = pd.DataFrame([sub.split("|") for sub in lines[1:-2]], columns=lines[0].split("|"))
     return df['Symbol'].tolist()
 
-def get_nyse_tickers():
+def tickers_nyse():
     url = 'http://www.nasdaqtrader.com/dynamic/symdir/otherlisted.txt'
     data = requests.get(url).text
     # The data is '|' separated and the last two lines are not needed
@@ -28,7 +28,14 @@ def get_nyse_tickers():
     nyse_df = df[df['Exchange'] == 'N']
     return nyse_df['ACT Symbol'].tolist()
 
-def get_amex_tickers():
+def tickers_dow():
+    url = 'https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average'
+    html = requests.get(url).text
+    df = pd.read_html(html, header=0, attrs = {'id': 'constituents'})[0]
+    tickers = df['Symbol'].tolist()
+    return tickers
+
+def tickers_amex():
     url = 'http://www.nasdaqtrader.com/dynamic/symdir/otherlisted.txt'
     data = requests.get(url).text
     # The data is '|' separated and the last two lines are not needed
