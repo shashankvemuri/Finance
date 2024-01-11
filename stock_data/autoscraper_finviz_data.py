ac3522ea8752b30db6d89330271a714217be30f4
shashankvemuri <shashank.vemuri1@gmail.com>
2024-01-11 01:47:13 -0500

    Update autoscraper_finviz_data.py

--- stock_data/autoscraper_finviz_data.py
+++ stock_data/autoscraper_finviz_data.py
@@ -1,28 +1,29 @@
-# Importing required modules
 from autoscraper import AutoScraper
 import pandas as pd
 
-# Setting display options to show all rows of dataframe
-pd.set_option('display.max_rows', None)
+# Fetches financial data for a list of tickers from Finviz using AutoScraper.
+def fetch_finviz_data(tickers, scraper_rule_path):
+    # Create an instance of AutoScraper
+    scraper = AutoScraper()
 
-# List of tickers to fetch data for
+    # Load scraper rules from the specified file path
+    scraper.load(scraper_rule_path)
+
+    # Iterate over the tickers and scrape data
+    for ticker in tickers:
+        url = f'https://finviz.com/quote.ashx?t={ticker}'
+        result = scraper.get_result(url)[0]
+
+        # Extract attributes and values
+        index = result.index('Index')
+        attributes, values = result[index:], result[:index]
+
+        # Create a DataFrame and display data
+        df = pd.DataFrame(zip(attributes, values), columns=['Attributes', 'Values'])
+        print(f'\n{ticker} Data:')
+        print(df.set_index('Attributes'))
+
+# Example usage
 tickers = ['SCHB', 'AMZN', 'AAPL', 'MSFT', 'TSLA', 'AMD', 'NFLX']
-
-# Creating an instance of AutoScraper and loading previously created scraper rules
-scraper = AutoScraper()
-scraper.load('../finviz_table')
-
-# Fetching data for each ticker
-for ticker in tickers:
-    url = f'https://finviz.com/quote.ashx?t={ticker}'
-
-    # Using AutoScraper to extract data from the URL
-    result = scraper.get_result(url)[0]
-
-    # Separating the extracted data into attributes and their corresponding values
-    index = result.index('Index')
-    df = pd.DataFrame(zip(result[index:], result[:index]), columns = ['Attributes', 'Values'])
-
-    # Displaying the extracted data for each ticker
-    print (f'\n{ticker} Data: ')
-    print (df.set_index('Attributes'))
+scraper_rule_path = '../finviz_table'
+fetch_finviz_data(tickers, scraper_rule_path)
