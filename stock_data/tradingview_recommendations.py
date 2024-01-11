9e9addaa9081f52470135e9b94358e8df16af6f8
shashankvemuri <shashank.vemuri1@gmail.com>
2024-01-11 01:48:40 -0500

    Update tradingview_recommendations.py

--- stock_data/tradingview_recommendations.py
+++ stock_data/tradingview_recommendations.py
@@ -1,37 +1,71 @@
-# Import dependencies
+# Importing necessary libraries
 import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from pandas_datareader import data as pdr
+import yfinance as yf
 import sys
 import os
 parent_dir = os.path.dirname(os.getcwd())
 sys.path.append(parent_dir)
 import tickers as ti
+yf.pdr_override()
 
-# Parameters
+# Define stock tickers and time interval
 tickers = ['DDOG', 'NVDA', 'PTON', 'RH', 'ROKU', 'SE', 'SQ', 'TSLA', 'TTD']
-interval = '1W'
+interval = '1D'
 
-# Lists of tickers
+# Getting lists of tickers from NASDAQ, NYSE, and AMEX
 nasdaq = ti.tickers_nasdaq()
 nyse = ti.tickers_nyse()
 amex = ti.tickers_amex()
 
-# List of valid time intervals
-type_intervals = ['1m', '5m', '15m', '1h', '4h', '1D', '1W', '1M']
+# Define valid time intervals
+type_intervals = ['1m', '5m', '15m', '30m', '1h', '2h', '4h', '1D', '1W', '1M']
 
-# Set up chromedriver
+# Set up Selenium WebDriver
 options = Options()
 options.add_argument("--headless")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# Loop through the tickers
+# Helper function to parse recommendation data
+def parse_recommendation(recommendation_elements, counter_elements, rec_index, sell_index, buy_index):
+    rec = recommendation_elements[rec_index].get_attribute('innerHTML')
+    sell_signals = int(counter_elements[sell_index].get_attribute('innerHTML'))
+    neutral_signals = int(counter_elements[sell_index + 1].get_attribute('innerHTML'))
+    buy_signals = int(counter_elements[buy_index].get_attribute('innerHTML'))
+    return rec, sell_signals, neutral_signals, buy_signals
+
+# Helper function to display recommendations
+def display_recommendations(analysis):
+    for key in analysis:
+        print(f"\n{key} Recommendation: ")
+        rec, sell, neutral, buy = analysis[key]
+        print(f"Recommendation: {rec}")
+        print(f"Sell Signals: {sell}")
+        print(f"Neutral Signals: {neutral}")
+        print(f"Buy Signals: {buy}")
+
+# Helper function to scrape tables
+def scrape_tables(html):
+    tables = pd.read_html(html, attrs = {'class': 'table-hvDpy38G'})
+    return tables[0], tables[1], tables[2]  # Oscillator, Moving Averages, and Pivots tables
+
+# Helper function to print tables
+def print_tables(oscillator_table, ma_table, pivots_table):
+    print("\nOscillator Table:")
+    print(oscillator_table)
+    print("\nMoving Average Table:")
+    print(ma_table)
+    print("\nPivots Table:")
+    print(pivots_table)
+
+# Process each ticker
 for ticker in tickers:
-    analysis = []
     try:
         # Determine the exchange of the ticker
         if ticker in nasdaq:
@@ -42,77 +76,49 @@ for ticker in tickers:
             exchange = 'AMEX'
         else:
             print(f"Could not find the exchange for {ticker}")
-        
+            continue
+
         # Get the current price of the ticker
         df = pdr.get_data_yahoo(ticker)
         price = round(df["Adj Close"][-1], 2)
-        
-        # Retrieve technical analysis data from TradingView
-        driver.get("https://www.tradingview.com/symbols/{}-{}/technicals".format(exchange, ticker))
+
+        # Open TradingView page for the ticker
+        driver.get(f"https://www.tradingview.com/symbols/{exchange}-{ticker}/technicals")
         time.sleep(1)
-        
-        # Print out the ticker, interval, and price
-        print('Ticker: ' + ticker)
+
+        # Display ticker, interval, and price information
+        print('\nTicker: ' + ticker)
         print('Interval: ' + interval)
         print('Price: ' + str(price))
-        
-        # Click on the specified time interval
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-        for type_interval, n in zip(type_intervals, numbers):
-            if interval == type_interval:
-                element = driver.find_element_by_xpath(f'//*[@id="technicals-root"]/div/div/div[1]/div/div/div[1]/div/div/div[{n}]')
-                element.click()
-            else:
-                continue
-        
-        time.sleep(1)
-        
-        # Overall Recommendation
-        recommendation_elements = driver.find_elements_by_class_name("speedometerSignal-pyzN--tL")
-        analysis.append(recommendation_elements[1].get_attribute('innerHTML'))
-        counter_elements = driver.find_elements_by_class_name("counterNumber-3l14ys0C")
-        analysis.append(int(counter_elements[3].get_attribute('innerHTML')))
-        analysis.append(int(counter_elements[4].get_attribute('innerHTML')))
-        analysis.append(int(counter_elements[5].get_attribute('innerHTML')))
-        df = pd.DataFrame.from_records([tuple(analysis)], columns=['Recommendation', '# of Sell Signals', '# of Neutral Signals', '# of Buy Signals'])
-        print('\nOverall Recommendation: ')
-        print(df.set_index('Recommendation').T)
-        
-        # Oscillator Recommendation
-        analysis.append(recommendation_elements[0].get_attribute('innerHTML'))
-        counter_elements = driver.find_elements_by_class_name("counterNumber-3l14ys0C")
-        analysis.append(int(counter_elements[0].get_attribute('innerHTML')))
-        analysis.append(int(counter_elements[1].get_attribute('innerHTML')))
-        analysis.append(int(counter_elements[2].get_attribute('innerHTML')))
-        df = pd.DataFrame.from_records([tuple(analysis[4:8])], columns=['Recommendation', '# of Sell Signals', '# of Neutral Signals', '# of Buy Signals'])
-        print ('\nOscillator Recommendation: ')
-        print (df.set_index('Recommendation').T)
-    
-        # Moving Average Recommendation
-        analysis.append(recommendation_elements[2].get_attribute('innerHTML'))
-        counter_elements = driver.find_elements_by_class_name("counterNumber-3l14ys0C")
-        analysis.append(int(counter_elements[6].get_attribute('innerHTML')))
-        analysis.append(int(counter_elements[7].get_attribute('innerHTML')))
-        analysis.append(int(counter_elements[8].get_attribute('innerHTML')))
-        df = pd.DataFrame.from_records([tuple(analysis[8:12])], columns=['Recommendation', '# of Sell Signals', '# of Neutral Signals', '# of Buy Signals'])
-        print ('\nMoving Average Recommendation: ')
-        print (df.set_index('Recommendation').T)
-        
-        # Use pandas.read_html to get tables
-        html = driver.page_source
-        tables = pd.read_html(html, attrs = {'class': 'table-1YbYSTk8'})
-        
-        # Set variable names to tables
-        oscillator_table = tables[0]
-        ma_table = tables[1]
-        pivots_table = tables[2]
-        
-        # Rename dataframe columns
-        oscillator_table.columns = ['Name', 'Value', 'Action']
-        ma_table.columns = ['Name', 'Value', 'Action']
-        pivots_table.columns = ['Pivot', 'Classic', 'Fibonacci', 'Camarilla', 'Woodie', 'DM']
-    except Exception as e:
-        print (f'Could not retrieve stats for {ticker} because {e}')
 
-# Close the chromedriver
+        # Switch to the specified interval on TradingView page
+        for type_interval in type_intervals:
+            if interval == type_interval:
+                # n = type_intervals.index(type_interval)
+                element = driver.find_element(By.XPATH, f'//*[@id="{interval}"]')
+                # print (len(element))
+                element.click()
+                break
+
+        time.sleep(1)
+
+        # Scrape and display Overall, Oscillator, and Moving Average Recommendations
+        recommendation_elements = driver.find_elements(By.CLASS_NAME, "speedometerText-Tat_6ZmA")
+        counter_elements = driver.find_elements(By.CLASS_NAME, "counterNumber-kg4MJrFB")
+        analysis = {
+            'Overall': parse_recommendation(recommendation_elements, counter_elements, 1, 3, 5),
+            'Oscillator': parse_recommendation(recommendation_elements, counter_elements, 0, 0, 2),
+            'Moving Average': parse_recommendation(recommendation_elements, counter_elements, 2, 6, 8)
+        }
+        display_recommendations(analysis)
+
+        # Scrape and display tables for Oscillator, Moving Averages, and Pivots
+        html = driver.page_source
+        oscillator_table, ma_table, pivots_table = scrape_tables(html)
+        print_tables(oscillator_table, ma_table, pivots_table)
+
+    except Exception as e:
+        print(f'Could not retrieve stats for {ticker} due to {e}')
+
+# Close the WebDriver
 driver.close()
